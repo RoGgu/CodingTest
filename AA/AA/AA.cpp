92c72898537b2e5ed8d42b3055a4fc88677fef79
AnSungHun <cjswo4577@gmail.com>
2022-01-12 23:34:48 +0900

    1. 1부터 N까지의 M의 배수합 풀이
    첫 번쨰 문제풀이 커밋

--- AA/AA/AA.cpp
+++ AA/AA/AA.cpp
@@ -1,6 +1,18 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+//#include<stdio.h>
+#include<iostream>
 
+using namespace std;
 int main() {
+	int n, m, i, sum = 0;
+	cin >> n >> m;
 
+	for (i = 1; i <= n; i++)
+	{
+		if (i % m == 0) // m으로 나눠서 나머지가 0이면 i는 m의 배수
+			sum += i;
+	}
+
+	cout << sum;
+	return 0;
 }
