1dc9ff02518db674931f3f6b7becbea37012ce60
AnSungHun <cjswo4577@gmail.com>
2022-01-13 22:42:00 +0900

    2. 자연수의 합 풀이 [O]
    2번 자연수의 합 문제에 대한 코드 풀이

--- AA/AA/AA.cpp
+++ AA/AA/AA.cpp
@@ -4,15 +4,19 @@
 
 using namespace std;
 int main() {
-	int n, m, i, sum = 0;
-	cin >> n >> m;
+	int a, b, sum, i = 0; // a : 입력 수 A, b : 입렵 수 B, sum : 총 수의 합, i : for문 체크 수
 
-	for (i = 1; i <= n; i++)
+	cin >> a>> b; // A, B 입력 받음
+
+	cout << a; // 맨 처음 수 A 출력 후 
+	sum = a; // A를 총합의 더한다.
+	for (i = a + 1; i <= b; i++) // (A+1)번 부터 돌면서 "+i" 출력하면서 총합에 더해준다.
 	{
-		if (i % m == 0) // m으로 나눠서 나머지가 0이면 i는 m의 배수
-			sum += i;
+		cout << '+' << i;
+		sum += i;
 	}
 
-	cout << sum;
+	cout << '=' << sum; // 마지막에 "=[총 수의 합]" 출력
+
 	return 0;
 }
