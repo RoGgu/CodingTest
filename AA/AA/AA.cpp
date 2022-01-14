087bd52605265493b50a6f19097248548902429c
AnSungHun <cjswo4577@gmail.com>
2022-01-14 09:45:42 +0900

    [O] 4. 나이차이
    4번 나이차이 문제에 대한 코드 풀이
    freopen을 통한 "input.txt" 입력 값 자동으로 읽어오는 기능 추가

--- AA/AA/AA.cpp
+++ AA/AA/AA.cpp
@@ -4,23 +4,20 @@
 
 using namespace std;
 int main() {
-	int n, i, sum = 0;
+	freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, i, a, max = -2147000000, min = 2147000000; // n : 총수의 갯 수 , i : for문 반복 횟 수, a 입력받은 i 번째 수 , max : 최대 값, max : 최솟 값
 
-	cin >> n;
-
-	cout << 1;
-	sum = 1;
-	for (i = 2; i < n; i++)
+	cin >> n; // 총 수의 갯 수 입력 받고
+	for (i = 0; i < n; i++)	// for문 돌면서 최대값 최솟값 구함
 	{
-		if (n % i == 0)
-		{
-			cout << " + ";
-			cout << i;
-			sum += i;
-		}
+		cin >> a;
+		if (a > max)
+			max = a;
+		else if (a < min)
+			min = a;
 	}
 
-	cout << " = " << sum;
+	cout << max - min; // 차이 출력
 
 	return 0;
 }
