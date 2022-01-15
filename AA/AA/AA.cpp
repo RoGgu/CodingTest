29596e432a050490844263e5383d01d2041f5ad2
AnSungHun <cjswo4577@gmail.com>
2022-01-15 13:53:10 +0900

    [O] 5. 나이 계산(문자열 컨트롤)
    5번 나이 계산(문자열 컨트롤) 문제에 대한 코드 풀이
    #include<stdio.h>를 추가로 std::cin, std::cout 대신 scanf, printf사용

--- AA/AA/AA.cpp
+++ AA/AA/AA.cpp
@@ -1,23 +1,29 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-#include<iostream>
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+//#include<iostream>
 
 using namespace std;
 int main() {
-	freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
-	int n, i, a, max = -2147000000, min = 2147000000; // n : 총수의 갯 수 , i : for문 반복 횟 수, a 입력받은 i 번째 수 , max : 최대 값, max : 최솟 값
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	char a[20];	// 주민등록 번호
+	int year, age;	// year : 출생연도, age : 실제 나이
 
-	cin >> n; // 총 수의 갯 수 입력 받고
-	for (i = 0; i < n; i++)	// for문 돌면서 최대값 최솟값 구함
+	scanf("%s", &a);	// 주민등록 번호 얻어옴
+	if (a[7] == '1' || a[7] == '2')	// 뒤에 첫번쨰 자리가 1,또는2면 1900년대생
 	{
-		cin >> a;
-		if (a > max)
-			max = a;
-		else if (a < min)
-			min = a;
+		year = 1900 + ((a[0] - 48) * 10 + (a[1] - 48));	// a[0], a[1]이 숫자로 보여도 아스키 코드값 48~57 까지라서 48빼줘야 진짜 숫자 값
+	}
+	else
+	{
+		year = 2000 + ((a[0] - 48) * 10 + (a[1] - 48));
 	}
 
-	cout << max - min; // 차이 출력
+	age = 2019 - year + 1;
+	printf("%d ", age);
+	if (a[7] == '1' || a[7] == '3')	// 뒷자리 첫 번쨰 수로 성별 비교
+		printf("M");
+	else
+		printf("W");
 
 	return 0;
 }
