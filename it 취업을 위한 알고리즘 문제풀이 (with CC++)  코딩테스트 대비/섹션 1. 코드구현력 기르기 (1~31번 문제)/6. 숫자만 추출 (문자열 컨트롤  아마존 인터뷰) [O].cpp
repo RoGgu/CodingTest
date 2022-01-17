32aa8ecc9877269ac293ae31a784e73c55771a46
AnSungHun <cjswo4577@gmail.com>
2022-01-17 22:52:38 +0900

    [O] 6. 숫자만 추출 (문자열 컨트롤 : 아마존 인터뷰)
    6. 숫자만 추출 (문자열 컨트롤 : 아마존 인터뷰) 문제에 대한 코드 풀이

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/6. 숫자만 추출 (문자열 컨트롤  아마존 인터뷰) [O].cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+
+using namespace std;
+int main() {
+	freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	char a[50]; // 입력 값
+	int i, cnt = 0, res = 0; // i : for문 카운트, cnt : 약수 갯 수, res : 숫자 만 출력
+	scanf("%s", &a);
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 48 && a[i] <= 57)	// 아스키 코드 값으로 0~9까지 찾아서 넣음
+		{
+			res = res * 10 + (a[i] - 48);	// 자릿 수 늘려주면서 수 더 해줌 (초반에 0들어오면 그대로라 첫 번쨰 0필터링)
+		}
+	}
+
+	printf("%d\n", res);	// 숫자만 출력
+	for (i = 1; i <= res; i++)	// 약수 갯 수구하기
+	{
+		if (res % i == 0)
+			cnt++;
+	}
+	printf("%d", cnt);	// 약수 갯 수 출력
+
+	return 0;
+}
+
