a8f5104b9f271f2965ba2e1f1f4d2f11a8ce391a
AnSungHun <cjswo4577@gmail.com>
2022-01-20 22:57:35 +0900

    7, 8번 문제풀이 코드 추가
    7. 영어 단어 복구(문자열 컨트롤) [O]
    8. 올바른 괄호(문자열 컨트롤) [O]
    두 문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/7. 영어 단어 복구(문자열 컨트롤) [O].cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+
+int main() {
+	freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	char a[101];	// a: 입력 값
+	char res[101];	// 정답 소문자화 문자열
+	int i, lenght = 0; // i: for문 카운터, lenght : res의 길이
+
+	// scanf는 띄어쓰기가 있으면 끊겨서 세팅한다
+	//scanf("%s", a);
+	gets_s(a);	// 그래서 gets_s 쓰면 '\n' 줄바꿈 나올떄가지의 데이터 정보 받아옴
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] != ' ')
+		{
+			if (a[i] >= 65 && a[i] <= 90) // 대문자 소문자화 아스키코드값 더해줌
+			{
+				res[lenght] = a[i] + 32;
+			}
+			else
+			{
+				res[lenght] = a[i];
+			}
+			lenght++;
+		}
+	}
+	res[lenght] = '\0'; // res의 마지막지점을 나타낼려고 \0 넣어줌
+
+	printf("%s", res);
+
+	return 0;
+}
---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/8. 올바른 괄호(문자열 컨트롤) [O].cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+
+int main() {
+	freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	char a[31];	// a : 입력 값
+	int i, check = 0; // for문카운트, check : 괄호체크 ( 일 때 1증가, ) 일 떄 1감소
+
+	scanf("%s", a);
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] == '(')
+			check++;
+		else
+			check--;
+
+		if (check < 0) //마이너스가 되면 현재 열린것 보다 닫은게 더 많아서 잘못된거임
+		{
+			break;
+		}
+	}
+
+	if (check == 0) // 정상적인 괄호라면 열고 닫는거 수 같아서 0이된다.
+		printf("YES");
+	else
+		printf("NO");
+
+
+	return 0;
+}
