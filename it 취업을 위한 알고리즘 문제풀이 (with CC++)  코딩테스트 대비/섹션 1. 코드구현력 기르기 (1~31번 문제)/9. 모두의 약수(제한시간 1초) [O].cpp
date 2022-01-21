b62e2b1dac1544c1615a80e56f4b4fa0f23ff7cc
AnSungHun <cjswo4577@gmail.com>
2022-01-21 22:18:33 +0900

    9번 문제풀이 코드 추가
    9. 모두의 약수(제한시간 1초) [O]
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/9. 모두의 약수(제한시간 1초) [O].cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+
+using namespace std;
+int main() {
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int cnt[50001] = { 0 };	// 1부터 돌면서 자신의 배수에 자기가 약수로 속하니까 약수 갯수 하나씩 추가할 배열
+	int n, i, j; // n : 입력 수 , for문 카운터 i , j
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = i; j <= n; j = j + i) // i의 배수 돌면서 더해준다
+		{
+			cnt[j]++;
+		}
+	}
+
+	for (int i = 1; i <= n; i++)	// 총 더해진 약수의 갯 수 출력
+		printf("%d ", cnt[i]);
+
+	return 0;
+}
