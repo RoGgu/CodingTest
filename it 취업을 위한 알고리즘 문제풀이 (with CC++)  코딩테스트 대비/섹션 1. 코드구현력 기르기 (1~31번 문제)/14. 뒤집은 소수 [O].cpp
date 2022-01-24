028b9696fae5f48019a3ac2842a56304d4857d2e
AnSungHun <cjswo4577@gmail.com>
2022-01-24 22:51:38 +0900

    14번 문제풀이 코드 추가
    14. 뒤집은 소수
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/14. 뒤집은 소수 [O].cpp
@@ -0,0 +1,51 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+
+int reverse(int x)
+{
+	int result = 0;
+
+	while (x > 0) // ex)3700 같이 뒤집으면 0037인데 앞에 00은 무시해줘야함
+	{
+		result = (result * 10) + (x % 10);
+		x = x / 10;
+	}
+
+	return result;
+}
+
+bool isPrime(int x)
+{
+	int i;
+	bool result = true;
+
+	for (i = 2; i < x; i++)
+	{
+		if (x % i == 0)
+		{
+			result = false;
+			break;
+		}
+	}
+	return result;
+}
+
+int main() {
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, num, reverseNum, i;
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &num);
+		reverseNum = reverse(num);	// 수를 뒤집는다
+
+		if (reverseNum == 1)	// 뒤집은 수가 1이면 1은 소수아니니까 넘긴다
+			continue;
+
+		if (isPrime(reverseNum) == true)// 소수인지 체크
+			printf("%d ", reverseNum);
+	}
+
+	return 0;
+}
