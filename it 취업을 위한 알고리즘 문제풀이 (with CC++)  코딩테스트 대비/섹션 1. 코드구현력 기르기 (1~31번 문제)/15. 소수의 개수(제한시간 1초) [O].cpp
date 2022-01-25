ad8e1c60e7d16db7fa5445a8ccf2fb49cc242475
AnSungHun <cjswo4577@gmail.com>
2022-01-25 22:56:10 +0900

    15번 문제풀이 코드 추가
    15. 소수의 개수(제한시간 1초)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/15. 소수의 개수(제한시간 1초) [O].cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+
+int main() {
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, i, j, flag, cnt = 0; // n : 입력 값, i j :for문 카운터, flag : 1이면 소수, cnt : 약수 갯 수 
+	scanf("%d", &n);
+
+	for (i = 2; i <= n; i++)
+	{
+		flag = 1;
+		for (j = 2; j * j <= i; j++)	// 소수 체크는 그 수의 제곱근까지만 돌아도 알 수 있다.
+		{
+			if (i % j == 0)
+			{
+				flag = 0;
+				break;
+			}
+		}
+
+		if (flag == 1)
+			cnt++;
+	}
+
+	printf("%d", cnt);
+}
