78f3c819117e8599ea5e7df7d7bede40bccaae1b
AnSungHun <cjswo4577@gmail.com>
2022-01-26 13:58:18 +0900

    17번 문제풀이 코드 추가
    17. 선생님 퀴즈
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/17. 선생님 퀴즈 [O].cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+
+int main() {
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, num, answer, i, sum;	// n : 학생 수, num : N까지의 합 할떄 N, answer : 학생 제출 답, i : for문 카운트, sum : N수까지의 합
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d %d", &num, &answer);
+		sum = (num * (num + 1)) / 2;	// n까지의 수의 합 공식 :  n(n+1)/2
+		if (sum == answer)
+			printf("YES\n");
+		else
+			printf("NO\n");
+	}
+
+	return 0;
+}
