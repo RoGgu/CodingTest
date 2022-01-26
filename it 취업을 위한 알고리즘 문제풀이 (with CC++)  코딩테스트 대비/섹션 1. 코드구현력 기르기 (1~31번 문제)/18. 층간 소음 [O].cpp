79b23408504fa7eb0ef5fa98b0b99bd91d6a2e2a
AnSungHun <cjswo4577@gmail.com>
2022-01-26 13:58:28 +0900

    18번 문제풀이 코드 추가
    18. 층간 소음
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/18. 층간 소음 [O].cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+
+int main() {
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, m, num, i, time = 0, max = -2147000000, result = -1;	// n : 측정 갯수, m : 경보음 기준치, num : 측정 소음, i : for문 카운트, time : 경보음 지속 시간, max : 최대 지속시간, result : 최종적으로 최대 경보음 지속시간(경보음 울린적 없을땐 -1)
+	scanf("%d %d", &n, &m);
+
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &num);
+		if (num > m)	// 경보음 기준치 넘는지 체크
+		{
+			time++;
+			if (time > max)	// 경보음 지속시간이 최대시간보다 높을 경우에만 최대시간으로 다시 세팅
+			{
+				max = time;
+				result = max;
+			}
+		}
+		else
+		{
+			time = 0;
+		}
+	}
+
+	printf("%d", result);
+	return 0;
+}
