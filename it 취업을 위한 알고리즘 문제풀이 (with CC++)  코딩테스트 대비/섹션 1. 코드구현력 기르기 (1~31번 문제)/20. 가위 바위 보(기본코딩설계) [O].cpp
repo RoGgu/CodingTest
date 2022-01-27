b18ecdf200c6af328291b09a6aaf980f7db554a5
AnSungHun <cjswo4577@gmail.com>
2022-01-27 15:53:06 +0900

    20번 문제풀이 코드 추가
    20. 가위 바위 보(기본코딩설계)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/20. 가위 바위 보(기본코딩설계) [O].cpp
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+//#include<iostream>
+
+using namespace std;
+int main() {
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, i, temp; // n : 총 횟수, i : for문 카운트, temp : 가위바위보 결과 값
+	int a[101], b[101]; // a : a가 낸것들, b : b가 낸것들
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+
+	for (i = 0; i < n; i++)
+		scanf("%d", &b[i]);
+
+	for (i = 0; i < n; i++) // 매 회마다 비교해서 가위바위보 승리공식으로 계산
+	{
+		temp = a[i] - b[i];
+		if (temp < 0)
+			temp += 3;
+
+		if (temp == 0)
+			printf("D\n");
+		else if (temp == 1)
+			printf("A\n");
+		else
+			printf("B\n");
+	}
+
+	return 0;
+}
