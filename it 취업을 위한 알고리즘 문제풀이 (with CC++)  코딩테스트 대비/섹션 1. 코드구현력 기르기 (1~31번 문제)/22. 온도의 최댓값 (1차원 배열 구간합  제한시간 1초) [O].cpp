3eace2df17b7b83357b1ac06c66bfc3481062d36
AnSungHun <cjswo4577@gmail.com>
2022-01-29 00:23:26 +0900

    22번 문제풀이 코드 추가
    22. 온도의 최댓값 (1차원 배열 구간합 : 제한시간 1초)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/22. 온도의 최댓값 (1차원 배열 구간합  제한시간 1초) [O].cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+
+
+int main() {
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int i, n, day, max = -2147000000, sum = 0; // i : for문 카운트, n : 총 갯수, day : 온도 재는연속날짜, max : 제일 높은 연속날짜온도, sum : 온도합 계산
+	scanf("%d", &n);
+	scanf("%d", &day);
+
+	std::vector<int>a(n);	// 미리 할당하는 배열 말고 동적 할당 vector사용
+	for (i = 0; i < n; i++) // vector에 온도 정보 입력
+		scanf("%d", &a[i]);
+
+	for (i = 0; i < day; i++) // 처음 온도합 재는곳까지 일단 계산
+	{
+		sum += a[i];
+	}
+	max = sum;	// 일단 max는 처음 나온 온도의 합
+
+	for (i = day; i < n; i++)	// 제일 오래된 날짜온도 빼고 새로운 날짜 온도 더하고 max비교
+	{
+		sum = sum + a[i] - a[i - day];
+		if (sum > max)
+			max = sum;
+	}
+
+	printf("%d", max);
+	return 0;
+}
