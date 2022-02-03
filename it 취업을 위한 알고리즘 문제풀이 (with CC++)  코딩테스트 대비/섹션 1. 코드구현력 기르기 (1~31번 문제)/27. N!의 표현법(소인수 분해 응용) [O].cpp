1ad67a004e9d4db45130b8d64bf29b7464161125
AnSungHun <cjswo4577@gmail.com>
2022-02-03 17:29:06 +0900

    27번 문제풀이 코드 추가
    27. N!의 표현법(소인수 분해 응용)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/27. N!의 표현법(소인수 분해 응용) [O].cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int i, j, n, temp; // i: for문 카운트, j : 소인수 나누기, n : 입력 수, temp : 소인수로 나눈 후 값
+	scanf("%d", &n);
+	vector<int> ch(n + 1); // 소인수 개수 체크 배열
+
+	for (i = 2; i <= n; i++)
+	{
+		temp = i;
+		j = 2;
+		while (true)
+		{
+			if (temp % j == 0)	// 2부터 나누면서 소인수 개수 체크
+			{
+				temp = temp / j;
+				ch[j]++;
+			}
+			else
+				j++;
+
+			if (temp == 1) // 1이면 이미 다나눠진거라서 다음수로 넘어감
+				break;
+		}
+	}
+
+	printf("%d! = ", n);
+	for (i = 2; i <= n; i++)
+	{
+		if (ch[i] != 0)
+			printf("%d ", ch[i]);
+	}
+
+	return 0;
+}
