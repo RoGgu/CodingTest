a4bd1ae412bf62dd09e1a5c8cf420a8a83ebb5be
AnSungHun <cjswo4577@gmail.com>
2022-02-03 17:42:18 +0900

    28번 문제풀이 코드 추가
    28. N!에서 0의 개수(소인수분해 응용)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/28. N!에서 0의 개수(소인수분해 응용) [O].cpp
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int i, j, n, temp, num2 = 0, num5 = 0, cnt = 0;; // i: for문 카운트, j : 소인수 나누기, n : 입력 수, temp : 소인수로 나눈 후 값, num2: 소인수 2의 개수, num5: 소인수 5의 개수, cnt :총 0의 개수
+	scanf("%d", &n);
+
+	for (i = 2; i <= n; i++)
+	{
+		temp = i;
+		j = 2;
+		while (true)
+		{
+			if (temp % j == 0)	// 2부터 나누면서 소인수 개수 체크
+			{
+				temp = temp / j;
+				if (j == 2)	// 소인수 2와 5의 갯수 카운트
+					num2++;
+				else if (j == 5)
+					num5++;
+			}
+			else
+				j++;
+
+			if (temp == 1) // 1이면 이미 다나눠진거라서 다음수로 넘어감
+				break;
+		}
+	}
+
+
+	if (num2 <= num5)	// 짝이 맞아야해서 둘 중 낮은 갯수로 출력
+		cnt = num2;
+	else
+		cnt = num5;
+
+	printf("%d", cnt);
+	return 0;
+}
