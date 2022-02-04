cbebcc0c712c082494d486af89391b342180bdcd
AnSungHun <cjswo4577@gmail.com>
2022-02-04 21:27:52 +0900

    29번 문제풀이 코드 추가
    29. 3의 개수는? (small : 구글 인터뷰)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/29. 3의 개수는 (small  구글 인터뷰) [O].cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<algorithm>
+
+using namespace std;
+int main() {
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int i, n, temp, count = 0; // i : for문 카운트, n : 입력 수, temp : 현재 3체크 수, count 총 3의 개수
+	scanf("%d", &n);
+
+	for (i = 3; i <= n; i++)
+	{
+		temp = i;
+		while (temp > 0)
+		{
+			if (temp % 10 == 3)	// 나머지가 3이면 3이있는거니까 체크
+				count++;
+			temp = temp / 10;	// 한자리씩 확인하면서 넘어간다.
+		}
+	}
+
+	printf("%d", count);
+	return 0;
+}
