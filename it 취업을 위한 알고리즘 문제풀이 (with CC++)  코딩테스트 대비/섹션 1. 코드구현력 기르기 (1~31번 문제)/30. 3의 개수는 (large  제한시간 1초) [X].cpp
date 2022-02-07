69822a1fb675dca8a90d9b2bb004d94d715d1c4b
AnSungHun <cjswo4577@gmail.com>
2022-02-07 23:23:05 +0900

    31번 문제풀이 코드 추가
    31. 탄화수소 질량
    문제 풀어서 풀이 코드 추가
    30번 코드 수정

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/30. 3의 개수는 (large  제한시간 1초) [X].cpp
@@ -0,0 +1,58 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+// 10의 자리에 3이 오는경우로 카운터
+
+// 3가지 경우로 체크 십의자리의 수가 3보다 큰경우, 같은경우, 작은경우
+
+// 십의 자리에 3이 처음나타는 경우는 30 - 30대는 3(0~9) 10개
+// 그담은 100대 130, 230... ----- 13(0~9) 10개, 23(0~9) 10개 .....
+
+// 구역 3개로 나눔 10의자리수는 cur 그왼쪽은 lt 그오른쪽 rt, k는 자릿수
+// ex)10의 자리 3 구할때 5367 -> 53: lt, 6 :cur, 7 :rt
+// 10의 자리의 3이 나오는 식 클경우 (lt+1)*10, 작을경우 lt*10 , 같을 경우 (lt * 10 ) + (rt + 1) ----- k 는 자릿 수만큼의 값
+
+// ex) 5367 자릿수별로 구분
+// lt	cur		rt		k	
+// 536	7		0		1		// 1의자리 3의 갯수 구할떄
+// 53	6		7		10		// 10의자리 ""
+// 5	3		67		100		// 100의자리 ""
+// 0	5		367		1000	// 1000의자리 ""
+
+
+int main() {
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, lt = 1, cur, rt, k = 1, res = 0;
+
+	scanf("%d", &n);
+
+	while (lt != 0)
+	{
+		lt = n / (k * 10);
+		cur = (n / k) % 10;
+		rt = n % k;
+		//printf("lt : %d,  cur : %d, rt : %d", lt, cur, rt);
+
+		if (cur > 3)
+		{
+			res = res + (lt + 1) * k;
+		}
+		else if (cur < 3)
+		{
+			res = res + (lt * k);
+		}
+		else
+		{
+			res = res + (lt * k) + (rt + 1);
+		}
+
+		k = k * 10;
+	}
+
+	printf("%d", res);
+	return 0;
+}
---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 1. 코드구현력 기르기 (1~31번 문제)/31. 탄화수소 질량 [O].cpp
@@ -0,0 +1,89 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	char a[10]; // 입력 값
+	int i, c = 0, h = 0, pos;	// i : for문 카운트  c :탄소 개수, h : 수소 개수, pos : 탄소 수소 인덱스 체크를위한 수
+	scanf("%s", &a);
+
+	if (a[1] == 'H')	// 두번째바로 H면 C는 숫자가 없다는 거니까 1
+	{
+		c = 1;
+		pos = 1;
+	}
+	else
+	{
+		for (i = 1; a[i] != 'H'; i++) // 탄소의 개수 카운트
+		{
+			c = c * 10 + (a[i] - 48);
+		}
+		pos = i;
+	}
+
+	if (a[pos + 1] == '\0')	// 수소바로 뒤에 끝나면 수소개수는 1
+		h = 1;
+	else
+	{
+		for (i = pos + 1; a[i] != '\0'; i++)
+		{
+			h = h * 10 + (a[i] - 48);
+		}
+	}
+
+	printf("%d", (c * 12 + h));
+	return 0;
+}
+
+
+
+//int main() {
+//	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+//	int index = 1, num = 0,res = 0;
+//	char a[10];
+//	char prev, cur;
+//
+//	scanf("%s", &a);
+//
+//	prev = a[0];
+//	while (true)
+//	{
+//		cur = a[index];
+//		if (cur == '\0')
+//			break;
+//
+//		if (cur >= 65 && cur <= 90)
+//		{
+//			if (num == 0)
+//				num = 1;
+//
+//			if (prev == 67)
+//				res = res + (12 * num);
+//			else
+//				res++;
+//
+//			prev = cur;
+//			num = 0;
+//		}
+//		else
+//			num = num * 10 + (cur - 48);
+//
+//		index++;
+//	}
+//
+//	if (num == 0)
+//		num = 1;
+//
+//	if (prev == 67)
+//		res = res + (12 * num);
+//	else
+//		res = res + num;
+//
+//	printf("%d", res);
+//	return 0;
+//}
