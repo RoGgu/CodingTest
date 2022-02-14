e694861005271abfb5bee2a1104ac363ef8052a1
AnSungHun <cjswo4577@gmail.com>
2022-02-14 23:13:53 +0900

    41번 문제풀이 코드 추가
    41. 연속된 자연수의 합(단순 수리)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/41. 연속된 자연수의 합(단순 수리) [O].cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, i, num, tmp, cnt = 0;
+	// n: 입력 받은 수, i: 만약 갯수에 맞게 떨어질떄 for문 카운트, num : 연속된 수의 개 수 , tmp : 연속된 수 뺸만큼의 남은 수, cnt : 총 나열된 자연수로 나온 수식 개수
+	scanf("%d", &n);
+
+
+	tmp = n;
+	num = 1; // 일단 1을 뺴준다 최소 2개 자리수니까
+	tmp--;
+	while (tmp > 0) // 구하는 공식 총 더하는 개수 를 원래 수에서 뺴고 남은 수를 개 수로 나눠서 나머지가 없으면 나열된 수로 표시되는 수
+	{
+		num++;
+		tmp -= num; // 나열되는 수만큼 뺸다
+
+		if (tmp % num == 0)	// 나열되는 수의 개수의 나머지가 0이면  개수를 몫으로 나누고 1부터 더 해지면 나열되는 수들
+		{
+			for (i = 1; i < num; i++)
+			{
+				printf("%d + ", tmp / num + i);
+			}
+			printf("%d = %d\n", (tmp / num + i), n);
+			cnt++;
+		}
+	}
+
+	printf("%d", cnt); // 개수 출력
+
+	return 0;
+}
