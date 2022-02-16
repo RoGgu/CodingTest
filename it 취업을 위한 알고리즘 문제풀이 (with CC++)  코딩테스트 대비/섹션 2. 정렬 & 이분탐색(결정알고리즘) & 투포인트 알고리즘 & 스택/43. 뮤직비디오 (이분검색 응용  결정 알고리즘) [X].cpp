59929483a72f8574f79e6fe3f0af1c76b98eeb8e
AnSungHun <cjswo4577@gmail.com>
2022-02-16 15:23:23 +0900

    43번 문제풀이 코드 추가 [X]
    43. 뮤직비디오 (이분검색 응용 : 결정 알고리즘)
    초반부 강의 듣고 품 나중에 다시 풀기
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/43. 뮤직비디오 (이분검색 응용  결정 알고리즘) [X].cpp
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+
+int main() {
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, m, i, lt = 1, rt = 0, mid, res, cnt = 0, tmp = 0;
+	scanf("%d %d", &n, &m);
+	vector<int> a(n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+		rt += a[i];	// 최대 메모리 값은 다 더한거니 최댓값 설정
+	}
+
+	while (lt <= rt) // 교차하는 순간 이분검색 끝
+	{
+		mid = (lt + rt) / 2; // 미들 값 설정 - 앨범 사이즈
+		cnt = 1;
+		tmp = 0;
+
+		for (i = 0; i < n; i++) // 곡 전부 돌면서 메모리 제한에 몇개에 DVD 필요한지 체크
+		{
+			if (tmp + a[i] > mid) // 메모리 제한 초과하면 새로운 DVD추기
+			{
+				tmp = a[i];
+				cnt++;
+			}
+			else
+				tmp += a[i]; // 아닐경우 DVD에 메모리 추가
+		}
+
+		if (cnt <= m) // DVD 개수제한 안에 들면 메모리 넣어주고 아래에 있는 메모리사이즈 다시 체크
+		{
+			res = mid;
+			rt = mid - 1;
+		}
+		else
+			lt = mid + 1;
+	}
+
+	printf("%d", res);
+	return 0;
+}
