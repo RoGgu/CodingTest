93f926ba54c37d7256982151dccc727f542b9ae9
AnSungHun <cjswo4577@gmail.com>
2022-02-17 20:51:58 +0900

    44번 문제풀이 코드 추가
    44. 마구간 정하기 (이분검색 응용 : 결정 알고리즘)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/44. 마구간 정하기 (이분검색 응용  결정 알고리즘) [O].cpp
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, m, i, lt = 1, mid, rt, pos, cnt, res = 0;
+	scanf("%d %d", &n, &m);
+	vector<int> a(n);
+	for (i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+
+	sort(a.begin(), a.end());
+
+	rt = a[n - 1];
+	while (lt <= rt) // 최대 거리 이분검색으로 정해서 말 배치 가능한지 체크
+	{
+		mid = (lt + rt) / 2;
+		cnt = 1; // 배치가능한 말의 수인데 처음 말은 0번째에 넣음
+		pos = a[0]; // 말사이의 거리 재기위해 마지막에 넣은 말의 위치
+		for (i = 1; i < n; i++)
+		{
+			if ((a[i] - pos) >= mid) // 기준점보다 거리가 멀면 배치가능한거
+			{
+				cnt++;
+				pos = a[i]; // 말으 여기다 넣고 마지막 말 넣은 위치 저장
+			}
+		}
+
+		if (cnt >= m) // 배치가능한 말의 수가 실제 말의 수보다 크거나 같으면 최대거리 가능
+		{
+			lt = mid + 1; // 더 큰 거리 가능한지 체크
+			res = mid;
+		}
+		else
+		{
+			rt = mid - 1; // 말 배치가 안돼면 최대거리 줄여줌
+		}
+	}
+
+	printf("%d", res);
+	return 0;
+}
