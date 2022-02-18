59be306594d31410768a19b34345a751b34ce08c
AnSungHun <cjswo4577@gmail.com>
2022-02-18 21:26:30 +0900

    45번 문제풀이 코드 추가
    45. 공주 구하기 (조세퍼스)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/45. 공주 구하기 (조세퍼스) [O].cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, k, i, cnt = 0, pos = 0, kCount = 1;
+	scanf("%d %d", &n, &k);
+	vector<int> a(n);
+
+	while (cnt < n - 1) // 마지막 왕자 남을때까지 반복
+	{
+		if (a[pos] != 1 && kCount == k)	// 이미제 제외도니 왕자 무시 && k 카운터랑 맞으면 그때 제거
+		{
+			a[pos] = 1;
+			kCount = 1;
+			cnt++;	// 제외된 왕자 수 카운트
+		}
+		else
+		{
+			if (a[pos] != 1)	// 만약 제외되지 않은 왕자면 kCount증가
+				kCount++;
+		}
+
+		pos++;
+		if (pos >= n) // 인덱스 범위 넘어가면 초기화
+			pos = 0;
+	}
+
+	for (i = 0; i < n; i++)	// 이제 제외되지 않은 왕자 출력
+	{
+		if (a[i] == 0)
+			printf("%d", i + 1);
+	}
+
+	return 0;
+}
