1e9fbadce6f51f7a8d87a83d5a0ce66111ce7e3d
AnSungHun <cjswo4577@gmail.com>
2022-02-20 22:55:12 +0900

    46번 문제풀이 코드 추가
    46. 멀티 태스킹 (카카오 "먹방" 문제 변형)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/46. 멀티 태스킹 (카카오 먹방 문제 변형) [O].cpp
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, k, i, second = 0, pos = 0, cnt = 0, total = 0;
+	scanf("%d", &n);
+	vector<int> a(n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+		total += a[i];
+	}
+	scanf("%d", &k);
+
+	if (total <= k) // 전체 작업 시간보다 정전되는 시간이 클경우 작업할꺼 없으니까 -1
+	{
+		printf("-1");
+		return 0;
+	}
+
+	while (true)
+	{
+		if (second == k)	// 현재 작업해야 하는데 정전되면 정전 후 현재작업 해야될것이 답이다
+		{
+			printf("%d", pos + 1);
+			break;
+		}
+
+		a[pos]--; // 현재 작업 시간감소
+		pos++;	// 다음 작업 위치 찾아놓기
+		if (pos == n)
+			pos = 0;
+		while (a[pos] == 0)	// 다음 작업해야할것을 찾는다 (작업시간 남아있는거)
+		{
+			pos++;
+			if (pos == n)
+				pos = 0;
+		}
+		second++; // 작업완료 후 시간증가
+	}
+
+	return 0;
+}
