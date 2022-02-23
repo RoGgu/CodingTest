7d7524f4aa6b4993b1bdb70893c4df1b09e6e532
AnSungHun <cjswo4577@gmail.com>
2022-02-23 14:25:12 +0900

    49번 문제풀이 코드 추가
    49. 쌓기 블록의 최댓값 (2차원 배열 응용)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/49. 쌓기 블록의 최댓값 (2차원 배열 응용) [O].cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, i, j, res = 0;;
+	scanf("%d", &n);
+	vector<int> front(n); // 앞에서 본 수
+	vector<int> right(n); // 오른쪽에서 본 수
+	for (i = 0; i < n; i++)
+		scanf("%d", &front[i]);
+	for (i = 0; i < n; i++)
+		scanf("%d", &right[i]);
+
+	vector<vector<int>> map(n, vector<int>(n));
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			// 앞 오른쪽 본것중에서 작은쪽의 수를 넣는다
+			if (front[j] <= right[i])
+				map[i][j] = front[j];
+			else
+				map[i][j] = right[i];
+
+			res += map[i][j];
+		}
+	}
+
+	printf("%d", res);
+	return 0;
+}
