aec9ba9cb452ff7c86b670f7778ce762396842d7
AnSungHun <cjswo4577@gmail.com>
2022-02-26 08:03:58 +0900

    52. Ugly Numbers (투포인트 알고리즘 응용)
    52. Ugly Numbers (투포인트 알고리즘 응용) [X]
    나중에 다시 풀기

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/52. Ugly Numbers (투포인트 알고리즘 응용) [X].cpp
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+int a[1501];
+int main() {
+	freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+
+	int n, i, p2, p3, p5, min = 0;
+	scanf("%d", &n);
+	a[1] = 1;
+	p2 = p3 = p5 = 1;
+	for (i = 2; i <= n; i++)
+	{
+		if (a[p2] * 2 < a[p3] * 3)
+			min = a[p2] * 2;
+		else
+			min = a[p3] * 3;
+		if (a[p5] * 5 < min)
+			min = a[p5] * 5;
+		if (min == a[p2] * 2)
+			p2++;
+		if (min == a[p3] * 3)
+			p3++;
+		if (min == a[p5] * 5)
+			p5++;
+		a[i] = min;
+	}
+	printf("%d\n", a[n]);
+	return 0;
+}
