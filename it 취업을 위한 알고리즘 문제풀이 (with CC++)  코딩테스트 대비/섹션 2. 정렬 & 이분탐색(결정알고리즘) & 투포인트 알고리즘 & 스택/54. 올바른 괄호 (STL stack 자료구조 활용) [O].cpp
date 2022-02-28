e2992802fb2c880fc603928390913669b75c0d64
AnSungHun <cjswo4577@gmail.com>
2022-02-28 21:47:08 +0900

    54번 문제풀이 코드 추가
    54. 올바른 괄호 (STL stack 자료구조 활용)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/54. 올바른 괄호 (STL stack 자료구조 활용) [O].cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+#include<stack>
+
+using namespace std;
+
+int main() {
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	stack<char> s;
+	char a[31];
+	int i;
+	scanf("%s", &a);
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] == '(') // 여는 괄호면 스택에 넣어줌
+			s.push('(');
+		else // 닫는 괄호면 '(' 남아있는게 있는지 체크
+		{
+			if (s.empty() == true) // 없으면'(' ')' 짝이 안 맞는거니 NO출력하고 끝냄
+			{
+				printf("NO");
+				exit(0);
+			}
+			s.pop(); // 있을경우 하나 제거
+		}
+	}
+
+	if (s.empty()) // '(' 남은거 없으면 짝 다 맞는거 
+		printf("YES");
+	else
+		printf("NO"); // 아니면 '(' 남아있는거라 ')' 모잘라 짝이 안맞으니 NO
+
+	return 0;
+}
