de9800709780e55e9e5abfa5d19dde4a1c0f43eb
AnSungHun <cjswo4577@gmail.com>
2022-03-02 22:38:23 +0900

    섹션 3 폴더 추가, 56번 재귀 기초문제 풀이 추가
    섹션 3 폴더 새로 추가
    56. 재귀함수 분석 (스택을 이용하는 재귀)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 3. 재귀 & 깊이넓이 우선탐색(DFS, BFS)/56. 재귀함수 분석 (스택을 이용하는 재귀) [O].cpp
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+#include<stack>
+
+using namespace std;
+
+int main() {
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	stack<int> s;
+	int n, i, count = 1, index = 0;
+	scanf("%d", &n);
+	vector<int> a(n);
+	for (i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+	vector<char> c; // P O 출력할꺼 담아둠ㄴ
+
+	while (count <= n)
+	{
+		// 스택 맨위에꺼가 기차 순서표랑 맞으면 빼준다
+		if (s.empty() == false && s.top() == count)
+		{
+			s.pop();
+			c.push_back('O');
+			count++;
+		}
+		else
+		{
+			// 아니면 스택에 넣는다
+			s.push(a[index]);
+			index++;
+			c.push_back('P');
+		}
+
+		// 더이상 넣을 수 있는게 없는데 빼야되는 수가 기차순서랑 안 맞으면
+		// 순서대로 운행 불가능
+		if (index == n && s.empty() == false && s.top() != count)
+		{
+			printf("impossible");
+			exit(0);
+		}
+	}
+
+	for (i = 0; i < c.size(); i++)
+		printf("%c", c[i]);
+
+	return 0;
+}
