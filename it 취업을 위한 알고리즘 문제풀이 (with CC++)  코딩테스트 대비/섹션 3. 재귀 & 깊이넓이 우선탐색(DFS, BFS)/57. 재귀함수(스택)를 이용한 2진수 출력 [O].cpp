df3ab5fcff86a1f2e28a079a89395a7b2e609680
AnSungHun <cjswo4577@gmail.com>
2022-03-03 14:14:05 +0900

    57번 문제풀이 코드 추가
    57. 재귀함수(스택)를 이용한 2진수 출력
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 3. 재귀 & 깊이넓이 우선탐색(DFS, BFS)/57. 재귀함수(스택)를 이용한 2진수 출력 [O].cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+void recur(int x) // 재귀 함수
+{
+	if (x == 0)
+		return;
+	recur(x / 2);
+	printf("%d", x % 2);
+}
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n;
+	scanf("%d", &n);
+	recur(n);
+	return 0;
+}
