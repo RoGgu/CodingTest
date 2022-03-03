47f0716a01be2b83841d5023f899e96aadad6ea9
AnSungHun <cjswo4577@gmail.com>
2022-03-03 14:08:35 +0900

    56번 문제풀이 코드 잘못 올라간거 수정
    56. cpp 수정

--- it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 3. 재귀 & 깊이넓이 우선탐색(DFS, BFS)/56. 재귀함수 분석 (스택을 이용하는 재귀) [O].cpp
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 3. 재귀 & 깊이넓이 우선탐색(DFS, BFS)/56. 재귀함수 분석 (스택을 이용하는 재귀) [O].cpp
@@ -2,48 +2,25 @@
 #include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
 #include<vector>
 #include<algorithm>
-#include<stack>
 
 using namespace std;
 
-int main() {
-	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
-	stack<int> s;
-	int n, i, count = 1, index = 0;
-	scanf("%d", &n);
-	vector<int> a(n);
-	for (i = 0; i < n; i++)
-		scanf("%d", &a[i]);
-	vector<char> c; // P O 출력할꺼 담아둠ㄴ
-
-	while (count <= n)
+void recur(int x) // 재귀 함수
+{
+	if (x == 0) // 특정 조건, 종료지점에 재귀 끝내게 만듬
+		return;
+	else // 아직 종료지점 전이라서 계속 함
 	{
-		// 스택 맨위에꺼가 기차 순서표랑 맞으면 빼준다
-		if (s.empty() == false && s.top() == count)
-		{
-			s.pop();
-			c.push_back('O');
-			count++;
-		}
-		else
-		{
-			// 아니면 스택에 넣는다
-			s.push(a[index]);
-			index++;
-			c.push_back('P');
-		}
-
-		// 더이상 넣을 수 있는게 없는데 빼야되는 수가 기차순서랑 안 맞으면
-		// 순서대로 운행 불가능
-		if (index == n && s.empty() == false && s.top() != count)
-		{
-			printf("impossible");
-			exit(0);
-		}
+		recur(x - 1); // 여기서 현재 상태 스택에 저장하고 다시 함수로 새로 들어감
+		printf("%d ", x); // 재귀가 끝나고 스택 최상단 1부터 출력되면서 모든 재귀함수 돌면서 마무리
 	}
+}
 
-	for (i = 0; i < c.size(); i++)
-		printf("%c", c[i]);
-
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n;
+	scanf("%d", &n);
+	recur(n);
 	return 0;
 }
