6b158d04f5fae58296bedf9eac23d9eaa3781504
AnSungHun <cjswo4577@gmail.com>
2022-03-05 21:14:29 +0900

    59번 문제풀이 코드 추가
    59. 부분집합 (MS 인터뷰 문제 : DFS 완전탐색) [X]
    나중에 다시 풀기

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 3. 재귀 & 깊이넓이 우선탐색(DFS, BFS)/59. 부분집합 (MS 인터뷰 문제  DFS 완전탐색) [X].cpp
@@ -0,0 +1,39 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int n;
+int ch[11];
+void DFS(int L)
+{
+	int i;
+	if (L == n + 1)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			if (ch[i] == 1)
+				printf("%d ", i);
+		}
+		puts(""); 		//printf("\n"); 줄바꿈 키워드
+
+	}
+	else
+	{
+		ch[L] = 1; // 왼쪽으로 갈때는 1로 켜줌
+		DFS(L + 1);
+		ch[L] = 0; // 오른쪽은 0으로 꺼줌
+		DFS(L + 1);
+	}
+}
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	scanf("%d", &n);
+	DFS(1);
+
+	return 0;
+}
