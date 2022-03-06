c2eb7982c210a65e499bba8004b0d524feba97f4
AnSungHun <cjswo4577@gmail.com>
2022-03-06 20:45:09 +0900

    60번 문제풀이 코드 추가
    60. 합이 같은 부분 집합 (아마존 인터뷰 문제 : DFS 완전탐색)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 3. 재귀 & 깊이넓이 우선탐색(DFS, BFS)/60. 합이 같은 부분 집합 (아마존 인터뷰 문제  DFS 완전탐색) [O].cpp
@@ -0,0 +1,52 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int n, total = 0;
+int a[11];
+bool found;
+
+void DFS(int L, int sum)
+{
+	if (sum > total / 2 || found == true) // 이미 총합의 반 지났으면 false, 이미 찾았으면 나머지 체크 X
+		return;
+
+	if (L >= n)
+	{
+		if (sum == (total - sum)) // 현재 집합 더한게 총합의 절반이면 나머지도 절반이니까 true
+		{
+			found = true;
+		}
+		return;
+	}
+	else
+	{
+		DFS(L + 1, sum + a[L]); // 왼쪽은 현재 index값 더 한거 넘겨주고
+		DFS(L + 1, sum); // 오른쪽은 안 더하고 넘겨줌
+	}
+
+}
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+		total += a[i];
+	}
+
+	DFS(0, 0);
+
+	if (found == true)
+		printf("YES");
+	else
+		printf("NO");
+
+
+	return 0;
+}
