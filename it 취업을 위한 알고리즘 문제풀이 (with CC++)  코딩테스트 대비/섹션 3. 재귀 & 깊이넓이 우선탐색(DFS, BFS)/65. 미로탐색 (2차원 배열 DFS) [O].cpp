d66eff7dae9729ac3343d0e6ac52f2322491a599
AnSungHun <cjswo4577@gmail.com>
2022-03-11 15:07:28 +0900

    65번 문제풀이 코드 추가
    65. 미로탐색 (2차원 배열 DFS)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 3. 재귀 & 깊이넓이 우선탐색(DFS, BFS)/65. 미로탐색 (2차원 배열 DFS) [O].cpp
@@ -0,0 +1,56 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+const int n = 7; // 맵 크기는 7로 고정
+int map[n][n] = { 0 }; // 맵
+int checkMap[n][n] = { 0 }; // 경로 체크 배열
+int cnt = 0; // 총 경로 수
+
+int dx[] = { 1, -1,  0 , 0 }; // 상화좌우 방향 배열
+int dy[] = { 0, 0, 1,  -1 };
+
+void DFS(int y, int x)
+{
+	if (x == n - 1 && y == n - 1) // 마지막 배열 도착지에 도착했으면 카운트
+	{
+		cnt++;
+		return;
+	}
+	else
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			int moveX = x + dx[i];
+			int moveY = y + dy[i];
+			if (moveX < n && moveY < n && moveX >= 0 && moveY >= 0) // 배열범위 벗어나면 패스
+				continue;
+			
+			if (map[moveY][moveX] == 0 && checkMap[moveY][moveX] == 0) // 갈 수 있는곳인지 체크, 이미 간곳인지 체크
+			{
+				checkMap[moveY][moveX] = 1; // 갔다고 기록
+				DFS(moveY, moveX);
+				checkMap[moveY][moveX] = 0; // 갔다 왔으니까 해제
+			}
+		}
+	}
+}
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+			scanf("%d", &map[i][j]);
+	}
+
+	checkMap[0][0] = 1;
+	DFS(0, 0);
+
+	printf("%d", cnt);
+	return 0;
+}
