1f190c48e8707be6f15c6b38e15d3240f54dea22
AnSungHun <cjswo4577@gmail.com>
2022-03-15 16:57:16 +0900

    67번 문제풀이 코드 추가
    67. 최소 비용 (그래프 DFS)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 3. 재귀 & 깊이넓이 우선탐색(DFS, BFS)/67. 최소 비용 (그래프 DFS) [O].cpp
@@ -0,0 +1,53 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<algorithm>
+#include<stack>
+
+using namespace std;
+
+int n, m;
+int map[21][21] = { };
+int ch[30]; // 경로 중복 체크
+int minG = 2147000000; // 가중치 최대 낮은 값
+
+void DFS(int v, int sum)
+{
+	if (v == n)
+	{
+		if (sum < minG) // 목표 도달했을때 가중치의 합 비교해서 작으면 바꿔줌
+		{
+			minG = sum;
+		}
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			if (map[v][i] > 0 && ch[i] == 0)
+			{
+				ch[i] = 1;
+				DFS(i, sum + map[v][i]); // 이동하는 가중치 값 더해줌
+				ch[i] = 0;
+			}
+		}
+	}
+}
+
+int main() {
+	///freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	scanf("%d %d", &n, &m);
+	int i, a, b, c;
+	for (i = 1; i <= m; i++)
+	{
+		scanf("%d %d %d", &a, &b, &c);
+		map[a][b] = c; // 가중치 값을 배열의 값으로 해줌
+	}
+
+	ch[1] = 1;
+	DFS(1, 0);
+
+	printf("%d", minG);
+
+	return 0;
+}
