8661ee7f29e3b9cd02ced33d860e37a2e0947901
AnSungHun <cjswo4577@gmail.com>
2022-03-21 22:19:19 +0900

    72번 문제풀이 코드 추가
    72. 공주 구하기 (조세퍼스 : 큐를 이용하는 방법)
    문제 풀어서 풀이 코드 추가

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 3. 재귀 & 깊이넓이 우선탐색(DFS, BFS)/72. 공주 구하기 (조세퍼스  큐를 이용하는 방법) [O].cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+// q의 왕자들 담아두고 돌면서 q에서 뺴서 뒤로 밀어넣는다
+// 그러다 k번째가 되면 그 왕자는 안넣고 k외치는 cnt 초기화
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int i, n, k, tmp, cnt = 1;
+	queue<int> q;
+	scanf("%d %d", &n, &k);
+
+	for (i = 1; i <= n; i++)
+		q.push(i);
+
+	while (!q.empty())
+	{
+		tmp = q.front();
+		q.pop();
+
+		if (cnt == k) // k번쨰 되면 퇴출되므로 cnt 1부터 다시 불러줌
+			cnt = 1;
+		else
+		{
+			cnt++;
+			q.push(tmp);
+		}
+	}
+
+	printf("%d", tmp); // 마지막 왕자가 tmp로 남아있으니 tmp출력
+	return 0;
+}
