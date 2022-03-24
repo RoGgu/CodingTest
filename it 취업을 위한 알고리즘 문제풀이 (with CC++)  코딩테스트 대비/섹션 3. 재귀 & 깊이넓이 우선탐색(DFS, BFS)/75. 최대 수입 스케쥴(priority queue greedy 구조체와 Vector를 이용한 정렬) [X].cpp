4730eff32df8e728b3750216f18e3f3bc25f0a90
AnSungHun <cjswo4577@gmail.com>
2022-03-24 23:55:05 +0900

    75번 문제풀이 코드 추가
    75. 최대 수입 스케쥴(priority queue greedy: 구조체와 Vector를 이용한 정렬) [X]
    풀이보고 문제 풀어서 나중에 다시 풀기

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 3. 재귀 & 깊이넓이 우선탐색(DFS, BFS)/75. 최대 수입 스케쥴(priority queue greedy 구조체와 Vector를 이용한 정렬) [X].cpp
@@ -0,0 +1,59 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+struct Data
+{
+	int money;
+	int when;
+	Data(int a, int b)
+	{
+		money = a;
+		when = b;
+	}
+	bool operator<(Data& b)
+	{
+		return when > b.when;
+	}
+};
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, i, j = 0, a, b, res = 0, max = -2147000000;
+	vector<Data>T;
+	priority_queue<int> pQ;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d %d", &a, &b);
+		T.push_back(Data(a, b));
+		if (b > max)
+			max = b;
+	}
+
+	sort(T.begin(), T.end());
+	for (i = max; i > 0; i--)
+	{
+		for (; j < T.size(); j++)
+		{
+			if (T[j].when < i)
+				break;
+			else if (T[j].when == i)
+				pQ.push(T[j].money);
+		}
+
+		if (!pQ.empty())
+		{
+			res += pQ.top();
+			pQ.pop();
+		}
+	}
+
+
+	printf("%d", res);
+	return 0;
+}
