6196c822e5b2891a8ec595df0434dd9fa60be8e4
AnSungHun <cjswo4577@gmail.com>
2022-03-26 22:05:02 +0900

    77번 문제풀이 코드 추가
    77. 친구인가?(Disjoint-set : Union&Find 알고리즘)
    문제 못 풀었다 다시 풀어보자

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 4. 그래프, DFS, BFS 관련 보충문제/77. 친구인가(Disjoint-set  Union&Find 알고리즘) [X].cpp
@@ -0,0 +1,56 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+// 다시 풀기 !!!!!!!!!!!!!!!!!!!
+
+int unf[1001];
+
+int Find(int v)
+{
+	if (v == unf[v])
+		return v;
+	else
+		unf[v] = Find(unf[v]);
+}
+
+void Union(int a, int b)
+{
+	a = Find(a);
+	b = Find(b);
+	if (a != b)
+		unf[a] = b;
+}
+
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n, m, i, j, a, b;
+	bool res = false;
+	scanf("%d %d", &n, &m);
+
+	for (i = 1; i <= n; i++)
+		unf[i] = i;
+
+	for (i = 1; i <= m; i++)
+	{
+		scanf("%d %d", &a, &b);
+		Union(a, b);
+	}
+
+	scanf("%d %d", &a, &b);
+	a = Find(a);
+	b = Find(b);
+
+	if (a == b)
+		printf("YES");
+	else
+		printf("NO");
+
+	return 0;
+}
