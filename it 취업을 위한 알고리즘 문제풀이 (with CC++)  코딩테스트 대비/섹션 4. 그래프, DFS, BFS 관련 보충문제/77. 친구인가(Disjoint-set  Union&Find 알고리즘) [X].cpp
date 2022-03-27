182d7cc8b675755d8a1c512df3cecca895b36b55
AnSungHun <cjswo4577@gmail.com>
2022-03-27 23:06:26 +0900

    78번 문제풀이 코드 추가
    78. 원더랜드 : Kruskal MST(최소스패닝트리) 알고리즘 : Union&Find 활용
    문제 풀어서 풀이 코드 추가
    77번 다시 풀고 주석 추가

--- it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 4. 그래프, DFS, BFS 관련 보충문제/77. 친구인가(Disjoint-set  Union&Find 알고리즘) [X].cpp
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 4. 그래프, DFS, BFS 관련 보충문제/77. 친구인가(Disjoint-set  Union&Find 알고리즘) [X].cpp
@@ -6,51 +6,46 @@
 
 using namespace std;
 
-// 다시 풀기 !!!!!!!!!!!!!!!!!!!
-
 int unf[1001];
 
 int Find(int v)
 {
-	if (v == unf[v])
+	if (v == unf[v]) // 자기 배열의 수가 나올때까지 찾는다
 		return v;
 	else
-		unf[v] = Find(unf[v]);
+		return unf[v] = Find(unf[v]); // 다시 반복으로 구할 필요 없도록 세팅다시 해준다.
 }
 
-void Union(int a, int b)
+void Unf(int a, int b)
 {
 	a = Find(a);
 	b = Find(b);
-	if (a != b)
+
+	if (a != b) // 만약 현재 다른 그룹에 있다면 a에 b의 값을 넣어서 그룹을 연결해준다.
 		unf[a] = b;
 }
-
 
 int main()
 {
 	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
-	int n, m, i, j, a, b;
-	bool res = false;
+	int n, m, i, a, b;
 	scanf("%d %d", &n, &m);
-
-	for (i = 1; i <= n; i++)
+	for (i = 1; i <= n; i++) // 자기 배열의 수 넣어줌
 		unf[i] = i;
 
-	for (i = 1; i <= m; i++)
+	for (i = 1; i <= m; i++) // 숫자 쌍 돌면서 연결 세팅
 	{
 		scanf("%d %d", &a, &b);
-		Union(a, b);
+		Unf(a, b);
 	}
 
 	scanf("%d %d", &a, &b);
-	a = Find(a);
+	a = Find(a); 
 	b = Find(b);
 
-	if (a == b)
+	if (a == b) // 두 수가 연결되어 같은 수가 나온다.
 		printf("YES");
 	else
 		printf("NO");
-
 	return 0;
 }
---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 4. 그래프, DFS, BFS 관련 보충문제/78. 원더랜드  Kruskal MST(최소스패닝트리) 알고리즘  Union&Find 활용 [O].cpp
@@ -0,0 +1,90 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+// 크루스칼 알고리즘 사용
+// 최소비용의 트리를 만들어낸다.
+
+// 가중치 값으로 오름차순 정렬하고
+// 차례대로 연결되어있는지 판단해서 연결 안 되어있다면 연결해주고 가중치 값 더해줌
+// 연결된 간선 수가 노드 -1 개면 전부 연결 된거로 판단하고 종룔
+
+struct ST // 가중치 그래프 저장을 위한 구조체
+{
+	int numA;
+	int numB;
+	int g;
+
+	ST(int a, int b, int c)
+	{
+		numA = a;
+		numB = b;
+		g = c;
+	}
+
+	bool operator<(const ST& st) const // 구조체 비교는 g 가중치로 오름차순 정렬 한다
+	{
+		return g < st.g;
+	}
+};
+
+int unf[30];
+
+int Find(int v)
+{
+	if (v == unf[v])
+		return v;
+	else
+		return unf[v] = Find(unf[v]);
+}
+
+bool Unf(int a, int b)
+{
+	a = Find(a);
+	b = Find(b);
+	if (a != b) // 새로 연결될 때만 가중치 더하기 위해서 bool 값 리턴
+	{
+		unf[a] = b; // 두 노드 연결
+		return true;
+	}
+	else
+		return false;
+}
+
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+
+	int n, m, i, a, b, g, sum = 0, cnt = 0;;
+	vector<ST> v;
+	scanf("%d %d", &n, &m);
+	for (i = 1; i <= n; i++)
+		unf[i] = i;
+
+	for (i = 1; i <= m; i++)
+	{
+		scanf("%d %d %d", &a, &b, &g);
+		v.push_back(ST(a, b, g));
+	}
+
+	sort(v.begin(), v.end()); // 가중치 값으로 오름차순 정렬
+
+	for (i = 0; i < m; i++)
+	{
+		if (Unf(v[i].numA, v[i].numB) == true) // 서로 연결되면 즉 연결이 안되어있던 상태였다면 가중치 더해줌
+		{
+			sum += v[i].g;
+			cnt++;
+			if (cnt == n - 1) // 간선의 개수는 노드 수 -1 이므로 다 연결되었다는 판단
+				break;
+		}
+	}
+
+	printf("%d", sum);
+	return 0;
+}
