bad78f92953a5d9e3f4f32a5bef6abc433482442
AnSungHun <cjswo4577@gmail.com>
2022-04-13 22:11:30 +0900

    [선수지식] 조합구하기
    - 조합 구하는 기본 코드

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 4. 그래프, DFS, BFS 관련 보충문제/[선수지식] 조합구하기.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+// 조합 구하기 nCr
+// ex) 6C4 = 6개의 수중 4개의 수 조합
+// 조합 구하는 코드는 많이 쓰여서 외우는것도 좋다.
+
+int n, r;
+int ch[20]; // 조합배열
+
+void DFS(int s, int L)
+{
+	if (L == r) // 원하는 조합의 원소 갯수가 충족되면 되면 출력한다
+	{
+		for (int i = 0; i < r; i++)
+		{
+			cout << ch[i] << " ";
+		}
+		cout << endl;
+	}
+	else
+	{
+		for (int i = s; i < n; i++)
+		{
+			ch[L] = i;
+			DFS(i + 1, L + 1);
+		}
+	}
+}
+
+int main()
+{
+	freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	cin >> n >> r;
+	DFS(0, 0);
+
+
+	return 0;
+}
