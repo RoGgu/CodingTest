844c2e80c699af17227ad24c2706a67dbcfccfd6
AnSungHun <cjswo4577@gmail.com>
2022-04-15 00:22:11 +0900

    88번 문제풀이 코드 추가
    88. 미로의 최단거리 통로(BFS 활용)
    문제 풀어서 풀이 코드 추가
    - BFS를 사용해서 미로 탈출 최단거리 구하기

---
+++ it 취업을 위한 알고리즘 문제풀이 (with CC++)  코딩테스트 대비/섹션 4. 그래프, DFS, BFS 관련 보충문제/88. 미로의 최단거리 통로(BFS 활용) [O].cpp
@@ -0,0 +1,64 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>	// cin,cout말고 이제 c에서 제공하는 sanf, printf 사용 - 속도가 조금 더 빠름
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cstdlib>
+
+using namespace std;
+
+// 미로 최단거리 탐색은 BFS로 찾는다
+
+int main()
+{
+	freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+	int n = 7, tmp, cnt = 0;
+	int dy[4] = { -1, 1,  0, 0, }; // 4방향 회전 상하좌우
+	int dx[4] = { 0, 0, -1, 1, };
+	vector<vector<int>> map(n, vector<int>(n));
+	vector<vector<int>> dis(n, vector<int>(n)); // 각 위치에 도달하는데 소요된 거리 세팅
+	queue<pair<int, int>> q;
+
+	for (int y = 0; y < n; y++)
+	{
+		for (int x = 0; x < n; x++)
+		{
+			cin >> tmp;;
+			map[y][x] = tmp;
+		}
+	}
+
+	q.push(make_pair(0, 0));
+	dis[0][0] = 0;
+
+	while (q.empty() == false)
+	{
+		int y = q.front().first;
+		int x = q.front().second;
+		q.pop();
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nextY = y + dy[i];
+			int nextX = x + dx[i];
+
+			if (nextY < 0 || nextY >= n || nextX < 0 || nextX >= n)
+				continue;
+
+			if (map[nextY][nextX] == 0 && dis[nextY][nextX] == 0)
+			{
+				dis[nextY][nextX] = dis[y][x] + 1; // next 위치를 발견한 발견한 위치까지에 소요된 거리 +1
+
+				q.push(make_pair(nextY, nextX));
+			}
+		}
+	}
+
+	if (dis[6][6] == 0) // 거리가 0 이면 도달 못한거니까 -1 표시
+		cout << -1 << endl;
+	else
+		cout << dis[6][6] << endl;
+
+	return 0;
+}
