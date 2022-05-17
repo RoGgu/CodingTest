374a5e57377d0ede130aa1d56f1237b0f2af67b5
AnSungHun <cjswo4577@gmail.com>
2022-05-17 22:29:19 +0900

    백준 1725폭발 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 풀지 못해서 풀이보고 풀었다.
    - stack을 이용해서 기존보다 낮은 막대 그래프 들어올떄 넓이 구하는 식으로 코드를 작성
    - stack 방식 말고 그래프 분할 정복으로 푸는방식도 있다. (추후작성)

---
+++ 백준 알고리즘/1725. 히스토그램 [X].cpp
@@ -0,0 +1,39 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+#include<stack>
+
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    freopen("input.txt", "rt", stdin);
+    int n, maxArea = 0;
+    cin >> n;
+    vector<int> h(n + 2, 0); // 맨 처음, 뒤는 0으로 채운다.
+    stack<int> s;
+
+    for (int i = 1; i <= n; i++) // 1부터 시작
+        cin >> h[i];
+
+    s.push(0); // 높이가 0인걸 기본으로 넣는다.
+
+    for (int i = 1; i <= n + 1; i++) // n+1까지 하는 이유는 히스토리 계속 증가했을 경우 while문 안 돌아서 마지막에 0 넣었다는 가정을 위해
+    {
+        while (s.empty() == false && h[s.top()] > h[i]) // 기존 높이보다 작은 높이 들어오면, 그높이보다 작거나 같아질때 까지 기존에 있던것들 넓이 계산
+        {
+            int tmpIdx = s.top(); // 현재 높아서 뺴야하는 막대 인덱스 저장
+            s.pop();
+            int height = h[tmpIdx]; //  높이
+            int width = (i - s.top() - 1); // 가로 길이 - 현재 tmpIdx 인덱스부터 i -1 까지 지금 막대 그래프보다 높이가 낮은건 없어서 전부 가로길이로 계산
+            int area = height * width; // 높이 * 너비 = 넓이
+
+            if (area > maxArea) // 최고 넓이 기록
+                maxArea = area;
+        }
+
+        s.push(i); // 자기보다 높은 막대기 다 빼고나서 자기자신 넣어줌
+    }
+
+    cout << maxArea << endl;
+    return 0;
+}
