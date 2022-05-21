4a1bcc841e6fd86bc8a50a9ec77993188d876540
AnSungHun <cjswo4577@gmail.com>
2022-05-21 21:58:36 +0900

    백준 1463. 1로 만들기 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - DP(다이나믹 프로그래밍) 방식으로 풀이 작성
    - 처음엔 BFS랑 다이나믹해서 풀었는데 너무 오래걸려서 수정했다.

---
+++ 백준 알고리즘/1463. 1로 만들기.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    freopen("input.txt", "rt", stdin);
+    int X, maxCnt = 2147000000;
+    cin >> X;
+    vector<int> dy(X + 1, 0);
+
+    // 2와 3일 경우 나누기 한번씩으로 바로 답이나온다.
+    dy[2] = 1;
+    dy[3] = 1;
+
+    for (int i = 4; i <= X; i++)
+    {
+        dy[i] = 1 + dy[i - 1]; // 일단 -1 했을떄 값 세팅
+        if (i % 3 == 0)
+            dy[i] = min(dy[i], 1 + dy[i / 3]); // -1햇을때랑 3으로 나눳을떄에 dy값 + 한번더 계싼하니까 1 더해준거랑 비교해서 작은거 세팅
+        if (i % 2 == 0)
+            dy[i] = min(dy[i], 1 + dy[i / 2]); // -1햇을때랑 2으로 나눳을떄에 dy값 + 한번더 계싼하니까 1 더해준거랑 비교해서 작은거 세팅
+    }
+
+    cout << dy[X] << endl;
+    return 0;
+}
