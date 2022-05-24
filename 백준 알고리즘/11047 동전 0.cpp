3706933edf038e6f920181f96f57409979e10a55
AnSungHun <cjswo4577@gmail.com>
2022-05-24 20:57:34 +0900

    백준  11047. 동전 0 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 그리드로 푸는 간단한 문제

---
+++ 백준 알고리즘/11047 동전 0.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    freopen("input.txt", "rt", stdin);
+    int n, k, cnt = 0;
+    vector<int> coin;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) // 동전 세팅
+    {
+        int tmp;
+        cin >> tmp;
+        coin.push_back(tmp);
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (k >= coin[i]) // 현재 동전으로 계산 가능하면 동전 카운트하고 값 뺀다.
+        {
+            k -= coin[i];
+            cnt++;
+        }
+    }
+
+    cout << cnt << endl;
+
+    return 0;
+}
