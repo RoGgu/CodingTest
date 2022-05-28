f62a98f7894ed17b26a821777315da23e52b40a7
AnSungHun <cjswo4577@gmail.com>
2022-05-28 17:25:39 +0900

    백준 2579. 계단오르기 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 다이나믹 풀이 문제인데 풀이보고 풀었다, 나중에 다시 풀어보자

---
+++ 백준 알고리즘/2579. 계단오르기 [X].cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+#include<map>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+    int n;
+    cin >> n;
+    vector<int> stairs(n);
+    vector<int> dy(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> stairs[i];
+
+    dy[0] = stairs[0];
+    dy[1] = max(stairs[1], stairs[0] + stairs[1]);
+    dy[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2]);
+
+    for (int i = 3; i < n; i++)
+    {
+        dy[i] = max(stairs[i] + dy[i - 2], stairs[i] + stairs[i - 1] + dy[i - 3]);
+
+    }
+
+    cout << dy[n - 1] << endl;
+    return 0;
+}
+
