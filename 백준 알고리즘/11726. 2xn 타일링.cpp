88446dd8c2e8027729a7ac10053fd64587b2ceb4
AnSungHun <cjswo4577@gmail.com>
2022-06-01 17:10:34 +0900

    백준 11726. 2xn 타일링 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 피보나치 수열문제처럼 푼다 (10007 나누기 까먹어서 처음에 틀렸다).

---
+++ 백준 알고리즘/11726. 2xn 타일링.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+#include<map>
+#include<set>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    int res = 0;
+    vector<int> dy;
+
+    // 피보나치로 푸는 문제
+    dy.push_back(1);
+    dy.push_back(2);
+
+    for (int i = 2; i < n; i++)
+    {
+        dy.push_back((dy[i - 1] + dy[i - 2]) % 10007); // 10007로 나눠준 값을 가져야한다.
+    }
+
+    cout << dy[n - 1] << endl;
+    return 0;
+}
+
