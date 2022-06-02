57c2949c7be128aadfd4c21cb7efaddfb914907f
AnSungHun <cjswo4577@gmail.com>
2022-06-02 19:50:25 +0900

    백준 11279. 최대 힙 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 최대 힙 자료구조 사용해서 문제풀이

---
+++ 백준 알고리즘/11279. 최대 힙.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include <string.h>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "rt", stdin);
+    int n, x;
+    priority_queue<int> pq;
+    scanf("%d", &n); // 속도 떄문에 scanf, printf 씀
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
+        if (x == 0)
+        {
+            if (pq.empty())
+                printf("0\n");
+            else
+            {
+                printf("%d\n", pq.top());
+                pq.pop();
+            }
+        }
+        else
+            pq.push(x);
+    }
+
+    return 0;
+}
