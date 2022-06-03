e0c20fe911bfc1f33668f1c77e43fd739e985331
AnSungHun <cjswo4577@gmail.com>
2022-06-03 23:07:14 +0900

    백준 17626. Four Squares 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 작은 수 부터 차레대로 제곱근 수 가장 작은거 세팅하면서 구한다.

---
+++ 백준 알고리즘/17626. Four Squares.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include <string.h>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    freopen("input.txt", "rt", stdin);
+    int n;
+    cin >> n;
+    vector<int>dp(n + 1, 0);
+    dp[1] = 1; //dp를  제곱근의 갯수 중 제일 작은 개수로 되는걸로 세팅
+
+    for (int i = 2; i <= n; i++)
+    {
+        int minn = 2147000000;
+        for (int j = 1; j * j <= i; j++)
+        {
+            int tmp = i - j * j;
+            minn = min(minn, dp[tmp]);
+        }
+        dp[i] = minn + 1;
+    }
+    cout << dp[n];
+    return 0;
+}
