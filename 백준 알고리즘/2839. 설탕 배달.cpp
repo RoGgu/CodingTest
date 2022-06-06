cc7c4bb7f5be870a693b0889d82c83700da40597
AnSungHun <cjswo4577@gmail.com>
2022-06-06 18:58:39 +0900

    백준 2839. 설탱 배달 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 두 가지 종건으로 우선 순위 큐 역순으로 사용해서 풀이

---
+++ 백준 알고리즘/2839. 설탕 배달.cpp
@@ -0,0 +1,53 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include <string.h>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    freopen("input.txt", "rt", stdin);
+    int n, cnt = 0;
+    cin >> n;
+    priority_queue<int, vector<int>, greater<int>> pq; // 가장 작은거부터 하는 우선순위 큐 사용
+
+    pq.push(n);
+    vector<int> ch(5001, 2147000000);
+    ch[n] = 0;
+
+    while (!pq.empty())
+    {
+        int tmp = pq.top();
+        pq.pop();
+
+        if (tmp - 5 == 0) // 5kg 포대 이용할경우
+        {
+            if (ch[0] > ch[tmp] + 1)
+                ch[0] = ch[tmp] + 1;
+        }
+        else if (tmp > 5 && ch[tmp - 5] > ch[tmp] + 1)
+        {
+            ch[tmp - 5] = ch[tmp] + 1;
+            pq.push(tmp - 5);
+        }
+
+        if (tmp - 3 == 0)  // 3kg 포대 이용할경우
+        {
+            if (ch[0] > ch[tmp] + 1)
+                ch[0] = ch[tmp] + 1;
+        }
+        else if (tmp > 3 && ch[tmp - 3] > ch[tmp] + 1)
+        {
+            ch[tmp - 3] = ch[tmp] + 1;
+            pq.push(tmp - 3);
+        }
+    }
+
+    if (ch[0] == 2147000000) // ch[0] 설정된게 없다면 5,3 포대 두 종류로 운반할 수 없는 경우
+        cout << -1 << endl;
+    else
+        cout << ch[0] << endl;
+
+    return 0;
+}
