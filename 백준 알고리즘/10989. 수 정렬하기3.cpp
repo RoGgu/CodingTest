40edc52ec78f3dc0fdb009efd656e03c98debf92
AnSungHun <cjswo4577@gmail.com>
2022-06-07 13:23:19 +0900

    백준 10989. 수 정렬하기 3 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 기존 sort방식 말고 수가 너무 많아서 카운터하는 방식으로 오름차순 출력

---
+++ 백준 알고리즘/10989. 수 정렬하기3.cpp
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+int ch[10001];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int tmp;
+        scanf("%d", &tmp);
+        ch[tmp] += 1;
+    }
+
+    for (int i = 1; i <= 10000; i++) // 입력 수가 너무 많아서 카운터하는 방식으로 수 출력
+    {
+        for (int j = 0; j < ch[i]; j++)
+            printf("%d\n", i);
+    }
+
+    return 0;
+}
