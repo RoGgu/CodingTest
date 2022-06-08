324b2c7162ae3854accbaeb628bb49c8a0a73da7
AnSungHun <cjswo4577@gmail.com>
2022-06-08 18:16:53 +0900

    백준 11053. 가장 긴 증가하는 부분 수열 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 다이나믹 방식으로 수 마다 개수 세팅하면서 풀이

---
+++ 백준 알고리즘/11053. 가장 긴 증가하는 부분 수열.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int n, res = 0;
+    cin >> n;
+    vector<int> v(n);
+    vector<int> dp(n, 1); // 해당 인덱스에 증가 수열 개수 세팅
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (v[i] > v[j])
+            {
+                if (dp[i] < dp[j] + 1)
+                    dp[i] = dp[j] + 1;
+            }
+        }
+
+        res = max(res, dp[i]);
+    }
+
+    cout << res << endl;
+    return 0;
+}
