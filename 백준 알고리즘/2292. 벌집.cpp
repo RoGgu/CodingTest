f51c7c290320ed2139ce3c91c75a883d2104a04a
AnSungHun <cjswo4577@gmail.com>
2022-06-08 18:31:45 +0900

    백준 2292. 벌집 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 수열 규칙 찾아서 푸는 간단한 문제

---
+++ 백준 알고리즘/2292. 벌집.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int n, tmp, res = 1;
+    cin >> n;
+
+    tmp = 1;
+    while (tmp < n)
+    {
+        tmp += 6 * res;
+        res++;
+    }
+
+    cout << res << endl;
+    return 0;
+}
