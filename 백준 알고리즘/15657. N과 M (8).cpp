bab3c28054c87f497b7921366ab3917e81272171
AnSungHun <cjswo4577@gmail.com>
2022-06-11 13:43:09 +0900

    백준 15657. N과 M (8) 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - DFS 탐색 풀이문제(중복 허용)

---
+++ 백준 알고리즘/15657. N과 M (8).cpp
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n, m;
+vector<int> nums;
+vector<int> res;
+
+void DFS(int idx, int cnt)
+{
+
+    if (cnt == m)
+    {
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << "\n";
+    }
+    else
+    {
+        for (int i = idx; i < n; i++)
+        {
+            res.push_back(nums[i]);
+            DFS(i, cnt + 1);
+            res.pop_back();
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+
+    cin >> n >> m;
+    nums.resize(n);
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+    sort(nums.begin(), nums.end());
+
+    DFS(0, 0);
+    return 0;
+}
