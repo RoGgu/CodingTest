ae7d9c27477f13948c7820a4d25b4512ffb714f7
AnSungHun <cjswo4577@gmail.com>
2022-06-12 10:00:45 +0900

    1978. 소수 찾기 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 소수 판별하는 간단한 문제

---
+++ 백준 알고리즘/1978. 소수 찾기.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+#include<map>
+#include<set>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+    int n, cnt = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        if (num == 1)
+            continue;
+        cnt += 1;
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                cnt -= 1;
+                break;
+            }
+        }
+    }
+
+    cout << cnt << "\n";
+    return 0;
+}
+
