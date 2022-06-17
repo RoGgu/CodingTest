f6a85d77295e24782f1104a6dfb3da3a0321eb9d
AnSungHun <cjswo4577@gmail.com>
2022-06-17 20:03:48 +0900

    N과 M (4) 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - DFS 탐색으로 수 증가하면서 출력

---
+++ 백준 알고리즘/N과 M (4).cpp
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+#include<vector>
+#include<queue>
+#include <unordered_map>
+using namespace std;
+
+int N, M;
+vector<int> res;
+
+void DFS(int num, int cnt)
+{
+    if (cnt == M)
+    {
+        for (int i = 0; i < res.size(); i++)
+        {
+            cout << res[i] << " ";
+        }
+        cout << "\n";
+    }
+    else
+    {
+        for (int i = num; i <= N; i++)
+        {
+            res.push_back(i);
+            DFS(i, cnt + 1);
+            res.pop_back();
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    cin >> N >> M;
+    DFS(1, 0);
+
+
+    return 0;
+}
