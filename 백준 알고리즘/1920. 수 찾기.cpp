7bcd585afcdbcb79b8287d8942e000aff3f5812c
AnSungHun <cjswo4577@gmail.com>
2022-06-20 23:16:22 +0900

    1920. 수 찾기 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 이진 탐색으로 풀이

---
+++ 백준 알고리즘/1920. 수 찾기.cpp
@@ -0,0 +1,64 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+#include<vector>
+#include<queue>
+#include <unordered_map>
+#include<algorithm>
+using namespace std;
+
+int N, M;
+int arr[100010];
+
+void binarySearch(int key)
+{
+    int start = 0;
+    int end = N - 1;
+    int mid;
+
+    while (end >= start)
+    {
+        mid = (start + end) / 2;
+        if (arr[mid] == key)
+        {
+            cout << 1 << "\n";
+            return;
+        }
+        else if (arr[mid] > key)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+    cout << 0 << "\n";
+    return;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+
+    cin >> N;
+    int temp;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> temp;
+        arr[i] = temp;
+    }
+
+    sort(arr, arr + N);
+    cin >> M;
+    for (int i = 0; i < M; i++)
+    {
+        cin >> temp;
+        binarySearch(temp);
+    }
+    return 0;
+}
