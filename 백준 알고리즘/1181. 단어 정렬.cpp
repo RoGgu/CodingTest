94a72c3a314cb3c26285c3af9d2152dad69b388a
AnSungHun <cjswo4577@gmail.com>
2022-06-22 22:49:08 +0900

    1181. 단어 정렬 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - string 정렬 조건 만들어서 sort한 후 중복체크해서 푸는 문제

---
+++ 백준 알고리즘/1181. 단어 정렬.cpp
@@ -0,0 +1,39 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+#include<vector>
+#include<queue>
+#include <unordered_map>
+#include<algorithm>
+using namespace std;
+
+int cmp(string a, string b) {
+	if (a.length() == b.length()) // 길이 같으면 사전순 
+		return a < b;
+	else
+		return a.length() < b.length(); // 길이 다르면 짦은거
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	freopen("input.txt", "rt", stdin);
+	int N;
+	cin >> N;
+	vector<string> words(N);
+	for (int i = 0; i < N; i++)
+	{
+		cin >> words[i];
+	}
+
+	sort(words.begin(), words.end(), cmp);
+	cout << words[0] << "\n";
+	for (int i = 1; i < N; i++)
+	{
+		if (words[i] == words[i - 1]) // 전 단어랑 같으면 중복된 거라 출력 안함
+			continue;
+		cout << words[i] << "\n";
+	}
+	return 0;
+}
