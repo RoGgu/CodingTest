b078a8f5241abca8d6470f83242922d021204f64
AnSungHun <cjswo4577@gmail.com>
2022-06-23 22:56:47 +0900

    2869. 달팽이는 올라가고 싶다 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 남는 거리 생각해서 공식 만들어 풀이

---
+++ 백준 알고리즘/2869. 달팽이는 올라가고 싶다.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+#include<vector>
+#include<queue>
+#include <unordered_map>
+#include<algorithm>
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	freopen("input.txt", "rt", stdin);
+	int A, B, V, cnt = 1;
+	cin >> A >> B >> V;
+
+	if (A < V) // 오르는 길이가 총 길이보다 짧다면 하루만에 오르기 떄문에 계산 안한다.
+	{
+		cnt += (V - A) / (A - B);
+
+		if ((V - A) % (A - B) != 0) // 나머지가 있다면 거리가 조금 모자란 경우라서 하루 추가
+			cnt += 1;
+	}
+
+	cout << cnt << "\n";
+	return 0;
+}
