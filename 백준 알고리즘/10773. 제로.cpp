8dbdd721a702b306236506fefef3635408490ee8
AnSungHun <cjswo4577@gmail.com>
2022-06-28 23:13:47 +0900

    10773. 제로 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - vector 자료구조 이용해서 풀이

---
+++ 백준 알고리즘/10773. 제로.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+#include<vector>
+#include<queue>
+#include <unordered_map>
+#include<algorithm>
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	freopen("input.txt", "rt", stdin);
+	int n, tmp, sum = 0;
+	vector<int> v;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> tmp;
+		if (tmp != 0)
+			v.push_back(tmp);
+		else
+			v.pop_back();
+	}
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		sum += v[i];
+	}
+
+	cout << sum << "\n";
+	return 0;
+}
