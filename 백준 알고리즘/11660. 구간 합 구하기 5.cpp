960ec31eb0ca5178115c666c7fb94fbaf6752833
AnSungHun <cjswo4577@gmail.com>
2022-07-02 22:33:02 +0900

    11660. 구간 합 구하기 5 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 다이나믹 세팅으로 구간별 합 세팅한 후 범위 계산해서 답 추출

---
+++ 백준 알고리즘/11660. 구간 합 구하기 5.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+#include<vector>
+#include<queue>
+#include <unordered_map>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	freopen("input.txt", "rt", stdin);
+	int n, m, tmp, res = 0;;
+	cin >> n >> m;
+	vector<vector<int>> dy(n + 1, vector<int>(n + 1, 0)); // 구간별 전부 더 했을때에 합을 세팅
+	for (int x = 1; x <= n; x++) // x가 행이고 y가 열이다.
+	{
+		for (int y = 1; y <= n; y++)
+		{
+			cin >> tmp;
+			dy[x][y] = dy[x - 1][y] + dy[x][y - 1] - dy[x - 1][y - 1] + tmp; // 현재 좌표 위까지 전부 더한거 + 왼쪽까지 전부 더한거 + 중복되는 부분 뺴고 입력 수 더하면 현재 좌표까지 더한 총 수가 세팅된다.
+		}
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+
+		res = dy[x2][y2] - dy[x1 - 1][y2] - dy[x2][y1 - 1] + dy[x1 - 1][y1 - 1]; // 현재 좌표까지의 합 - x1-1 까지의합 - y1-1까지의 합 + 죽복되서 뺴진 부분 더한다.
+
+		cout << res << "\n";
+	}
+
+	return 0;
+}
