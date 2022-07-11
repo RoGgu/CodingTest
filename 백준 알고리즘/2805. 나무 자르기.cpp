f84b621147e809d69ce22dd8756b86384e69eb37
AnSungHun <cjswo4577@gmail.com>
2022-07-11 10:40:39 +0900

    2805. 나무 자르기 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 이분탐색으로 문제 풀이

---
+++ 백준 알고리즘/2805. 나무 자르기.cpp
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<stack>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    long long n, m, max = -1;
+    long long start, end, mid, sum;
+    cin >> n >> m;
+
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    sort(v.begin(), v.end());
+
+    start = 0;
+    end = v[n - 1];
+
+    while (start <= end) {
+        sum = 0;
+        mid = (start + end) / 2;
+
+        for (int i = 0; i < n; i++)
+            if (v[i] - mid > 0)
+                sum += v[i] - mid;
+
+
+        if (sum >= m)
+        {
+            start = mid + 1;
+            if (mid > max)
+                max = mid;
+        }
+        else end = mid - 1;
+    }
+
+    cout << max;
+    return 0;
+}
