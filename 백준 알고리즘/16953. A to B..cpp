7d5c29e91554dd47561581945289913315c73ac1
AnSungHun <cjswo4577@gmail.com>
2022-07-20 23:46:41 +0900

    16953. A → B 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 너비우선 탐색으로 풀이

---
+++ 백준 알고리즘/16953. A to B..cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<stack>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	freopen("input.txt", "rt", stdin);
+	int A, B;
+	int ans = -1;
+	cin >> A >> B;
+	queue<pair<long long int, int> > q;
+	q.push(make_pair(A, 1));
+
+	while (!q.empty())
+	{
+		long long int k = q.front().first;
+		int n = q.front().second;
+		q.pop();
+
+		if (k == B)
+		{
+			ans = n;
+			break;
+		}
+
+		if (k * 2 <= B)
+			q.push(make_pair(k * 2, n + 1));
+		if (k * 10 + 1 <= B)
+			q.push(make_pair(k * 10 + 1, n + 1));
+	}
+
+	printf("%d", ans);
+	return 0;
+}
