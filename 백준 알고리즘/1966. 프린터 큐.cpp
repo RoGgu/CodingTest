371d12c331e0e599312865971718869e2f618a14
AnSungHun <cjswo4577@gmail.com>
2022-07-24 22:13:25 +0900

    1966. 프린터 큐 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 우선순위 큐 이용해서 풀이

---
+++ 백준 알고리즘/1966. 프린터 큐.cpp
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include <map>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int count = 0;
+    int test_case;
+    cin >> test_case;
+    int n, m, ipt;
+    for (int i = 0; i < test_case; ++i) 
+    {
+        count = 0;
+        cin >> n >> m;
+        queue<pair<int, int>> q;
+        priority_queue<int> pq; // 우선순위 큐
+        for (int j = 0; j < n; ++j) 
+        {
+            cin >> ipt;
+            q.push({ j, ipt });
+            pq.push(ipt);
+        }
+        while (!q.empty()) 
+        {
+            int index = q.front().first;
+            int value = q.front().second;
+            q.pop();
+            if (pq.top() == value) 
+            {
+                pq.pop();
+                ++count;
+                if (index == m)
+                {
+                    cout << count << endl;
+                    break;
+                }
+            }
+            else q.push({ index,value });
+        }
+    }
+}
