721a2edfaf5522d64de7de4ff2455cca8ac11906
AnSungHun <cjswo4577@gmail.com>
2022-07-25 23:22:44 +0900

    11286. 절대값 힙 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 우선순위 큐에 비교 함수 정의해서 풀이

---
+++ 백준 알고리즘/11286. 절대값 힙.cpp
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include <map>
+using namespace std;
+
+struct cmp {
+    bool operator()(int a, int b)
+    {
+        if (abs(a) == abs(b))
+            return a > b;
+        return abs(a) > abs(b);
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+
+    priority_queue<int, vector<int>, cmp> pq;
+    int N;
+    cin >> N;
+    int in = 0;
+    while (N--)
+    {
+        cin >> in;
+        if (in == 0)
+        {
+            if (pq.empty())
+            {
+                cout << 0 << "\n"; continue;
+            }
+
+            cout << pq.top() << "\n"; pq.pop();
+        }
+        else
+        {
+            pq.push(in);
+        }
+    }
+}
