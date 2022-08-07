db499c57d0e103476ffeb9a9f8fa4d1860d8268d
AnSungHun <cjswo4577@gmail.com>
2022-08-07 19:58:36 +0900

    3052. 나머지 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - map key값 중복 안되는거 이용해서 나머지 갯 수 구하는 식으로 풀이

---
+++ 백준 알고리즘/3052. 나머지.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin); // input.txt 파일 읽기
+    map<int, int> m;
+
+    int tmp;
+    for (int i = 0; i <= 10; i++)
+    {
+        cin >> tmp;
+        m.insert({ tmp % 42, 1 }); // map은 키 중복 안되서 새로운 나머지 값 나올때만 insert 작동
+    }
+
+    cout << m.size(); // 중복된 값이 없기 때문에 세팅된 key값이 나머지 개수
+
+    return 0;
+}
+
