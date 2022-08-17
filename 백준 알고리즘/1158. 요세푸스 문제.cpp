3283ca6b61c045d6983a03e2f1094deb9cd4b0e9
AnSungHun <cjswo4577@gmail.com>
2022-08-17 22:57:43 +0900

    1158. 요세푸스 문제 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - q 자료구조 이용해서 풀이

---
+++ 백준 알고리즘/1158. 요세푸스 문제.cpp
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int N, K, count = 1;
+    cin >> N >> K;
+    queue<int> q;
+    vector<int> v;
+    for (int i = 1; i <= N; i++)
+    {
+        q.push(i);
+    }
+
+    while (!q.empty())
+    {
+        int tmp = q.front();
+        q.pop();
+
+        if (count == K)
+        {
+            v.push_back(tmp);
+            count = 1;
+        }
+        else
+        {
+            q.push(tmp);
+            count++;
+        }
+    }
+
+    cout << "<" << v[0];
+    for (int i = 1; i < v.size(); i++)
+    {
+        cout << ", " << v[i];
+    }
+    cout << ">";
+
+    return 0;
+}
