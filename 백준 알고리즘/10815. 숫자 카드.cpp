aaa03c551c3d58e9a602066e7b5193c6c7d33e2f
AnSungHun <cjswo4577@gmail.com>
2022-08-19 00:03:47 +0900

    10815. 숫자 카드 문제 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 숫자 체크 하는 부분 map 자료구조 이용해서 풀이

---
+++ 백준 알고리즘/10815. 숫자 카드.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int n, m, tmp;
+    cin >> n;
+    map<int, int> chMap;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tmp;
+        chMap.insert(make_pair(tmp, 1));
+    }
+
+    cin >> m;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> tmp;
+        if (chMap.find(tmp) != chMap.end())
+        {
+            cout << "1 ";
+        }
+        else
+            cout << "0 ";
+    }
+
+
+    return 0;
+}
