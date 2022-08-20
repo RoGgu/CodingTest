cb76a4e7e7b015b8faf9b0f358f6794bcdfad3d2
AnSungHun <cjswo4577@gmail.com>
2022-08-20 23:18:07 +0900

    1427. 소트인사이드 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - vector sort 조건 내림차순으로 해서 풀이

---
+++ 백준 알고리즘/1427. 소트인사이드.cpp
@@ -0,0 +1,39 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+bool cmp(int a, int b)
+{
+    return a > b;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int n;
+    cin >> n;
+    vector<int> v;
+    while (n > 0) // 한 자리 씩 배열에 세팅
+    {
+        v.push_back(n % 10);
+        n = n / 10;
+    }
+
+    sort(v.begin(), v.end(), cmp); // 내림차순 조건 함수로 내림차순 정렬
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i];
+    }
+
+    return 0;
+}
