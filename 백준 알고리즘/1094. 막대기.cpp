59e0b15f28d3aa75687e2470b7a2551917ede14c
AnSungHun <cjswo4577@gmail.com>
2022-08-21 22:31:01 +0900

    1094. 막대기 문제 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 이진수 구하는 방식으로  풀이

---
+++ 백준 알고리즘/1094. 막대기.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int X;
+    cin >> X;
+
+    // 절반으로 나눠서 더해진 나머지 개 수를 구한다.
+    // 이진수 표현에서 1의 개수
+
+    int ans = 0;
+    while (X > 0)
+    {
+        if (X % 2 == 1)
+        {
+            ans++;
+        }
+        X /= 2;
+    }
+
+    cout << ans;
+    return 0;
+}
