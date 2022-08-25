c38e224ff317ce57c4d314bb44a291565f78f73c
AnSungHun <cjswo4577@gmail.com>
2022-08-25 22:52:24 +0900

    15649. N과 M (1) 문제 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 10974. 모든 순열와 같은 방식으로 DFS 탐색으로 푸는데 M 개수 채워지면 출력해서 푸는 방식

---
+++ 백준 알고리즘/15649. N과 M (1).cpp
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+int n, m;
+
+void DFS(vector<int>& ch, vector<int>& v)
+{
+    if (v.size() == m) // 현재 배열에 저장된 v 수가 m 개수 만큼 충족해지면 쌓인 순서대로 순열 출력
+    {
+        for (int i = 0; i < v.size(); i++)
+        {
+            cout << v[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    else
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (ch[i] == 0)
+            {
+                ch[i] = 1;
+                v.push_back(i);
+                DFS(ch, v);
+                ch[i] = 0;
+                v.pop_back();
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    cin >> n >> m;
+    vector<int> v;
+    vector<int> ch(n + 1, 0);
+
+    DFS(ch, v);
+
+    return 0;
+}
