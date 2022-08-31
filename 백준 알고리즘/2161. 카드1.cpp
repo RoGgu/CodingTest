09ef259b2cddda49d4a13217da64e46ddc2cff96
AnSungHun <cjswo4577@gmail.com>
2022-08-31 18:14:22 +0900

    2161. 카드1 문제 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - q 자료구조 이용해서 간단하게 풀이

---
+++ 백준 알고리즘/2161. 카드1.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int N;
+    queue<int> q;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        q.push(i);
+    }
+
+    while (q.size() != 1)
+    {
+        cout << q.front() << " ";
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+
+    cout << q.front();
+    return 0;
+}
