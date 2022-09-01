44b304d71161281543ffc767e5b390bab298ab7f
AnSungHun <cjswo4577@gmail.com>
2022-09-02 08:04:15 +0900

    4153. 직각삼각형 문제 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 직각 삼각형 조건 이용해서 풀이

---
+++ 백준 알고리즘/4153. 직각삼각형.cpp
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	freopen("input.txt", "rt", stdin);
+	int x, y, z;
+	int temp = 0;
+
+	while (true)
+	{
+		cin >> x >> y >> z;
+		if (x == 0 && y == 0 && z == 0)
+			break;
+
+		if (x > y)
+		{
+			temp = y;
+			y = x;
+			x = temp;
+		}
+		if (y > z)
+		{
+			temp = z;
+			z = y;
+			y = temp;
+		}
+
+		if (z * z == x * x + y * y) 
+			cout << "right\n";
+		else
+			cout << "wrong\n";
+
+	}
+
+	return 0;
+}
