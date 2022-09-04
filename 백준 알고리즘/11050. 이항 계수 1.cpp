77dd8eafbb87e7558c16438e23fe1451d12844f9
AnSungHun <cjswo4577@gmail.com>
2022-09-04 21:20:49 +0900

    11050. 이항 계수 1 문제 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 이항 계수 공식 팩토리얼 이용해서 풀이

---
+++ 백준 알고리즘/11050. 이항 계수 1.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+
+int Factorial(int n)
+{
+    if (n == 1 || n == 0)
+        return 1;
+
+
+    int res = 1;
+    for (int i = n; i >= 1; i--)
+    {
+        res *= i;
+    }
+
+    return res;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int n, k, res = 0;
+    cin >> n >> k;
+
+    // 이항계수 공식 n! / ((n-k)! * k!) 를 팩토리얼 이용해서 풀이
+    res = Factorial(n) / (Factorial(n - k) * Factorial(k));
+
+    cout << res;
+    return 0;
+}
