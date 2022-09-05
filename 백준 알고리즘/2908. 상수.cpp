74ad92e56636bb59701e05a9d2cecf64bc95756c
AnSungHun <cjswo4577@gmail.com>
2022-09-05 22:08:05 +0900

    2909. 상수 문제 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 숫자 뒤집은 후 비교 해서 간단하게 풀이

---
+++ 백준 알고리즘/2908. 상수.cpp
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int A, B;
+    cin >> A >> B;
+    int rA = 0, rB = 0;
+
+    while (A != 0)
+    {
+        rA = rA * 10 + A % 10;
+        A = A / 10;
+    }
+
+    while (B != 0)
+    {
+        rB = rB * 10 + B % 10;
+        B = B / 10;
+    }
+
+    if (rA > rB)
+    {
+        cout << rA;
+    }
+    else
+        cout << rB;
+
+    return 0;
+}
