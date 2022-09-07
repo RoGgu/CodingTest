f67cdc7b4d94cad4129e4cd35abf0d7f9cb09a7d
AnSungHun <cjswo4577@gmail.com>
2022-09-07 14:38:19 +0900

    1193. 분수찾기 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 수열 규칙 파악해서 푸는 간단한 문제

---
+++ 백준 알고리즘/1193. 분수찾기.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	freopen("input.txt", "rt", stdin);
+	int N;
+	cin >> N;
+
+	int i = 1;
+	while (N > i)  // N이 몇번쨰 대각선에 속해 있는지 찾는다.
+	{
+		N -= i;
+		i++;
+	}
+
+	// 속한 대각선이 짝수인지 홀수인지에 따라서 다르게 출력
+	if (i % 2 == 1)
+		cout << i + 1 - N << '/' << N << endl;
+	else
+		cout << N << '/' << i + 1 - N << endl;
+
+	return 0;
+}
