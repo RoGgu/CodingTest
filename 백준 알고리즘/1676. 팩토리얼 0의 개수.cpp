21ad1916ebc2e2ed93fb795281398754a809f98b
AnSungHun <cjswo4577@gmail.com>
2022-09-08 23:35:21 +0900

    1676. 팩토리얼 0의 개수 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 5의 제곱 값을 이용해서 0의 개수 구하는 공식으로 풀이

---
+++ 백준 알고리즘/1676. 팩토리얼 0의 개수.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int N;
+    cin >> N;
+
+    int res = 0;
+    int fiveN = 5; // 현재 5의 제곱 체크 수
+
+    while (N >= fiveN)
+    {
+        res += (N / fiveN);
+        fiveN = fiveN * 5; // 5를 곱해줘서 다음 5의 제곱수로 체크
+    }
+
+    cout << res;
+    return 0;
+}
