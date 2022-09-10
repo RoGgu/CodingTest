89c1fec53706094f05d5907935466f22dcbe8616
AnSungHun <cjswo4577@gmail.com>
2022-09-10 23:04:51 +0900

    10870. 피보나치 수 5 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 피보나치를 재귀가 아닌 다이나믹 방식으로 풀이

---
+++ 백준 알고리즘/10870. 피보나치 수 5.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+
+int dp[100] = { 0,1 }; // 0과 1은 기본 값 세팅
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int n;
+    cin >> n;
+
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    cout << dp[n];
+    return 0;
+}
