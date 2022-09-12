e120c3b5db28d0599fb566e5865458b4a360164b
AnSungHun <cjswo4577@gmail.com>
2022-09-12 16:14:32 +0900

    2675. 문자열 반복 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 문자열 각 문자 반복 적용 시키는 간단한 문제

---
+++ 백준 알고리즘/2675. 문자열 반복.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int T, n;
+    string s;
+    cin >> T;
+
+    for (int i = 0; i < T; i++) // 테스트 개수 T
+    {
+        cin >> n;
+        cin >> s;
+
+        for (int j = 0; j < s.size(); j++)
+        {
+            for (int h = 0; h < n; h++) // 각 문자를 n번 반복
+            {
+                cout << s[j];
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
