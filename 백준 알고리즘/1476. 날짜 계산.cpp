7a72ede5f96a76f10ab9de5c2d02b460dcf33040
AnSungHun <cjswo4577@gmail.com>
2022-09-16 18:13:17 +0900

    1476. 날짜 계산 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 특정 조건을 만족하는 수가 나오는 조건 성립해서 풀이

---
+++ 백준 알고리즘/1476. 날짜 계산.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int E, S, M, year = 1;
+    cin >> E >> S >> M;
+
+    while (true)
+    {
+        if ((year - E) % 15 == 0 && (year - S) % 28 == 0 && (year - M) % 19 == 0) // 세가지 조건의 배수가 되는 수가 정답 년도이다.
+            break;
+        else
+            ++year;
+    }
+
+    cout << year;
+    return 0;
+}
