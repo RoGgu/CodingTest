a5e26c566c4bbff825e7f5598efef2c09e900a2d
AnSungHun <cjswo4577@gmail.com>
2022-09-19 23:34:48 +0900

    11004. K번쨰 수 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 오름차순 정렬해서 풀이

---
+++ 백준 알고리즘/11004. K번째 수.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    sort(v.begin(), v.end());
+    cout << v[k - 1];
+
+    return 0;
+}
