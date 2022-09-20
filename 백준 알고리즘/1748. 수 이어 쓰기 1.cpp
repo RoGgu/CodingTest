23e4a519e504ab6ace72c056fac0062795be9596
AnSungHun <cjswo4577@gmail.com>
2022-09-20 23:37:27 +0900

    1748. 수 이어 쓰기 1 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 자리수 구하는 공식을 통해서 풀이

---
+++ 백준 알고리즘/1748. 수 이어 쓰기 1.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int n, res = 0;
+    cin >> n;
+
+    // n 까지의 각 자리 수의 합을 구한다.
+    // 1의 자리수는 n -10 +1,
+    // 10의 자리수는 n - 100 + 1
+    // 100의 자리수는 n - 1000 + 1
+    // 이런식으로 공식을 통해서  n 까지의 각 자리 수의 합을 구한다.
+    for (int i = 1; i <= n; i = i * 10)
+    {
+        res += n - i + 1;
+    }
+
+    cout << res;
+    return 0;
+}
