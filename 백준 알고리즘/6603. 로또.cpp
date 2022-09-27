93577ee60304303c4393bd0c3d4e7bf4d1b8f24f
AnSungHun <cjswo4577@gmail.com>
2022-09-27 19:59:41 +0900

    6603. 로또 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - DFS탐색으로 오름차순으로 숫자 6개 정보 뽑아서 출력

---
+++ 백준 알고리즘/6603. 로또.cpp
@@ -0,0 +1,65 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+int k;
+vector<int> s;
+vector<int> path; // 뽑은 숫자 정보들
+
+void DFS(int idx, int cnt)
+{
+    if (cnt == 6) // 숫자 6개가 완성되면 출력한다
+    {
+        for (int i = 0; i < path.size(); i++)
+            cout << path[i] << " ";
+        cout << "\n";
+    }
+    else
+    {
+        for (int i = idx + 1; i < s.size(); i++)
+        {
+            path.push_back(s[i]);
+            DFS(i, cnt + 1);
+            path.pop_back();
+        }
+    }
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+
+    while (true)
+    {
+        cin >> k;
+        if (k == 0)
+            break;
+
+        s.resize(k);
+        path.clear();
+        for (int i = 0; i < k; i++)
+            cin >> s[i];
+
+        for (int i = 0; i < k; i++)
+        {
+            path.push_back(s[i]);
+            DFS(i, 1);
+            path.pop_back();
+        }
+
+        cout << "\n"; // 테스트 한 줄 끝나면 줄 바꿈
+    }
+
+    return 0;
+}
