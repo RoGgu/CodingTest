8504784604b26664a0fcededb4164ef44b5bcfe3
AnSungHun <cjswo4577@gmail.com>
2022-10-07 17:23:12 +0900

    15651. N과 M (3) 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - DFS방식으로 풀이

---
+++ 백준 알고리즘/15651. N과 M (3).cpp
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+
+int N, M;
+vector<int> res;
+
+void DFS(int num, int cnt)
+{
+    if (cnt == M)
+    {
+        for (int i = 0; i < res.size(); i++)
+        {
+            cout << res[i] << " ";
+        }
+        cout << "\n";
+    }
+    else
+    {
+        for (int i = 1; i <= N; i++)
+        {
+            res.push_back(i);
+            DFS(i, cnt + 1);
+            res.pop_back();
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    cin >> N >> M;
+    DFS(1, 0);
+
+
+    return 0;
+}
