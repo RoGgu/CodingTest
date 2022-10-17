19a329ed714ad60fafac2e3b073e497a49b62d9b
AnSungHun <cjswo4577@gmail.com>
2022-10-17 23:42:45 +0900

    2822. 점수 계산 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - vector<pair<int,int>> 자료구조로 정렬해서 풀이

---
+++ 백준 알고리즘/2822. 점수 계산.cpp
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int sum = 0;
+    vector<pair<int, int>> vp(8);
+    vector<int> res;
+    for (int i = 0; i < 8; i++)
+    {
+        cin >> vp[i].first;
+        vp[i].second = i + 1;
+    }
+
+    sort(vp.begin(), vp.end()); // 점수로 오름차순 정렬
+    for (int i = 0; i < 5; i++) // 상위 5개 점수 더하고 번호 세팅
+    {
+        sum += vp[7 - i].first;
+        res.push_back(vp[7 - i].second);
+    }
+
+    cout << sum << "\n"; // 상위 5개점수 합 출력
+
+    sort(res.begin(), res.end()); // 상위 5개 문제번호 오름차순 정렬
+    for (int i = 0; i < 5; i++)
+    {
+        cout << res[i] << " "; // 문제 번호 출력
+    }
+
+    return 0;
+}
