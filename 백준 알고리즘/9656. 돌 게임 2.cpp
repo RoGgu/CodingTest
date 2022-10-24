6a956de74f50809d1c5373e413268d21d691ccea
AnSungHun <cjswo4577@gmail.com>
2022-10-24 21:31:53 +0900

    9656. 돌 게임 2 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 다이나믹 프로그래밍을 이용하여 풀이

---
+++ 백준 알고리즘/9656. 돌 게임 2.cpp
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+int dp[1001];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int N;
+    cin >> N;
+
+    // 0 : 창근이 승, 1 : 상근이 승
+    dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 0;
+
+    for (int i = 4; i <= N; i++)
+    {
+        // i - 1 혹은 i - 3 의 턴인 사람이 무조건 지도록 되어 있어서 해당 턴이 아닌 사람이 i번쨰에 이긴다.
+        if (dp[i - 1] == 0 || dp[i - 3] == 0) // i-1, i-3이 상근이 턴이라 i번째엔 창근이가 이긴다.
+            dp[i] = 1;
+        else
+            dp[i] = 0;
+    }
+
+
+    if (dp[N] == 1)
+        cout << "SK";
+    else
+        cout << "CY";
+
+    return 0;
+}
