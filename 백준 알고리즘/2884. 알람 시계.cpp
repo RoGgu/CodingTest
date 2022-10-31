7446a56b88e3de70151613714cb764089ad62708
AnSungHun <cjswo4577@gmail.com>
2022-10-31 23:23:23 +0900

    2884. 알람 시계 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 시간 계산법 이용해서 풀이

---
+++ 백준 알고리즘/2884. 알람 시계.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int H, M;
+    cin >> H >> M;
+
+    if (M >= 45)
+    {
+        M -= 45;
+    }
+    else
+    {
+        // 분이 모자랄 경우 시에서 1 빼준다
+        // 시가 0~24이여야 해서 24로 나눠준다.
+        H = (H + 23) % 24;
+        M += 15;
+    }
+
+    cout << H << " " << M;
+    return 0;
+}
