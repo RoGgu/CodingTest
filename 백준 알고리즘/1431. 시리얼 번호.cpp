8973d4e44e805d8af309fb9a736e71c7652bccba
AnSungHun <cjswo4577@gmail.com>
2022-11-01 22:28:43 +0900

    1431. 시리얼 번호 문제풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 정렬 조건 적용한 함수 만들어서 풀이

---
+++ 백준 알고리즘/1431. 시리얼 번호.cpp
@@ -0,0 +1,85 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+bool Compare(const string& s1, const string& s2)
+{
+    if (s1.size() == s2.size())
+    {
+        int sum1 = 0, sum2 = 0;
+
+        for (int i = 0; i < s1.size(); i++)
+        {
+            if (48 <= s1[i] && s1[i] <= 57)
+            {
+                sum1 += (s1[i] - 48);
+            }
+
+            if (48 <= s2[i] && s2[i] <= 57)
+            {
+                sum2 += (s2[i] - 48);
+            }
+        }
+
+        if (sum1 == sum2) // 숫자들 합 같을 경우 사전순으로 정렬
+        {
+            for (int i = 0; i < s1.size(); i++)
+            {
+                if (s1[i] == s2[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    return s1[i] < s2[i];
+                }
+            }
+        }
+        else
+        {
+            // 숫자 합을 정렬, 합이 작은게 앞으로 온다
+            if (sum1 < sum2)
+                return true;
+            else
+                return false;
+        }
+    }
+    else // 사이즈 다를 경우 적은게 앞으로 온다
+    {
+        if (s1.size() < s2.size())
+            return true;
+        else
+            return false;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int n;
+    cin >> n;
+    vector<string> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end(), Compare); // 정렬적용
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << "\n";
+    }
+
+    return 0;
+}
