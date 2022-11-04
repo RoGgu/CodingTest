d9a172ad415f9d22304e8dc7fcaa32f23a88f80d
AnSungHun <cjswo4577@gmail.com>
2022-11-04 15:54:21 +0900

    4948. 베르트랑 공준 문제 풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 에라토스테네스의 체로 소수 체크하는 방식 이용해서 풀이

---
+++ 백준 알고리즘/4948. 베르트랑 공준.cpp
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+const int maxNum = 246913; // 입력 최대 수
+int arr[maxNum]; // 소수는 0, 아니면 1
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int n;
+    int cnt = 0;
+
+    // 에스토스테네스의 체로 최대 수까지 소수 뺴곤 다 1으로 변경
+    for (int i = 2; i <= sqrt(maxNum); i++)
+    {
+        if (arr[i] == 1)
+            continue;
+
+        for (int j = 2 * i; j <= maxNum; j += i)
+        {
+            arr[j] = 1;
+        }
+    }
+
+    while (true)
+    {
+        cnt = 0;
+        cin >> n;
+        if (n == 0) // n이 0이면 종료
+            break;
+
+        // n+1 부터 2n 까지의 소수 수 체크
+        for (int i = n + 1; i <= n * 2; i++)
+        {
+            if (arr[i] == 0)
+                cnt++;
+        }
+
+        cout << cnt << "\n";
+    }
+
+    return 0;
+}
