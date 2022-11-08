eb8ad73d0fed7b14859ab409a691c41fb39cf4d3
AnSungHun <cjswo4577@gmail.com>
2022-11-08 23:42:19 +0900

    1138. 한 줄로 서기 [X] 문제 풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 규칙에 따라 순서대로 알맞은 배열 값에 세팅하는 간단한 문제지만, 문제 해석을 하지 못해서 풀이보고 이해 후 해결했다.

---
+++ 백준 알고리즘/1138. 한 줄로 서기 [X].cpp
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int N, left;
+    cin >> N;
+    int arr[10] = { 0 }; // 0은 빈자리
+
+    // 키가 1부터 시작해서 왼쪽에 몇명있는지 입력을 받는다
+    // 키가 1부터 시작하니 왼쪽에 키큰 사람에 숫자만큼 한칸 씩 오른족으로 가는데 이미 다른 사람이 자리에 있는 경우 오른쪽으로 한 칸 더 간다.
+    for (int i = 0; i < N; i++)
+    {
+        cin >> left;
+
+        for (int j = 0; j < N; j++)
+        {
+            if (left == 0 && arr[j] == 0) // 왼쪽에 키가 큰사람이 있는만큼 오른쪽을 이동하다가 빈자리 있으면 그자리에 세팅
+            {
+                arr[j] = i + 1;
+                break;
+            }
+            else if (arr[j] == 0) // 이미 다른 사람이 자리에 있는경우 카운트 하지 않는다
+            {
+                --left;
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
