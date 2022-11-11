cd162f12f13ff9b0f017524926c62b59acd9c691
AnSungHun <cjswo4577@gmail.com>
2022-11-11 23:58:37 +0900

    2477. 참외밭 [X] 문제 풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 포함하지 않는 작은 사각형 넓이를 구해서 큰 사각형에서 뺴서 전체적인 넓이 구하는 방식의 문제
    - 포함하지 않는 작은 사각형을 구하지 못 해 처음에 풀지 못했다

---
+++ 백준 알고리즘/2477. 참외밭 [X].cpp
@@ -0,0 +1,56 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+using namespace std;
+
+int N;
+int arr[6][2];  // 주어지는 방향, 길이쌍
+int cnt[5]; // 방향 별 등장 횟수 저장
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int s = 1; // 작은 사각형의 넓이
+    int b = 1; // 큰 사각형의 넓이
+
+    // 입력 받기
+    cin >> N;
+    for (int i = 0; i < 6; i++)
+    {
+        cin >> arr[i][0] >> arr[i][1];
+        cnt[arr[i][0]]++;
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+
+        if (cnt[arr[i][0]] == 1)
+        {
+            // 한 번씩 등장하는 방향이다
+            b *= arr[i][1]; // 큰 사각형의 너비/높이이다
+            continue;
+        }
+
+        // 시작점이 다르기 떄문에 modulo (나머지)연산 사용
+        int ni = (i + 1) % 6;
+        int nni = (i + 2) % 6;
+        // 현재(I)랑 다다음거(nni)랑 같으면 그 사이에 있는 것(ni)이 작은 사각형의 너비/높이이다
+        if (arr[i][0] == arr[nni][0])
+        {
+            s *= arr[ni][1];
+        }
+    }
+
+    // 큰 사각형의 넓이 - 작은 사각형의 넓이로 총 육각형의 넓이 구하고 평당 옥수수 수 곱한다
+    printf("%d", (b - s) * N);
+    return 0;
+}
