ec3ec00c2ee16409330f50c59c9f688213a3cac2
AnSungHun <cjswo4577@gmail.com>
2022-11-19 23:26:51 +0900

    13699. 점화식 문제 풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 다이나믹 프로그래밍방식으로 풀이, 숫자범위 생각해서 long long타입 선언 주의

---
+++ 백준 알고리즘/13699. 점화식.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+
+using namespace std;
+
+long long dp[36]; // 숫자 범위생각해서 long long 타입 선언
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int n;
+    cin >> n;
+
+    dp[0] = 1; // 0일 경우 1이다
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            dp[i] += dp[j - 1] * dp[i - j]; // 점화식 그대로 공식으로 풀이
+        }
+    }
+
+    cout << dp[n];
+    return 0;
+}
