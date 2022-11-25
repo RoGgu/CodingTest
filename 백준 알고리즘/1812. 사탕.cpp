4979c1548d229cc1f9a1be0340cb799c07ec2d8f
AnSungHun <cjswo4577@gmail.com>
2022-11-25 23:27:04 +0900

    1812. 사탕 문제 풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 사탕 총 개수 구한 후 2칸씩 띄워서 더하는 식으로 풀이

---
+++ 백준 알고리즘/1812. 사탕.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	freopen("input.txt", "rt", stdin);
+	int n;
+	int arr[1000], sum = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+		sum += arr[i];
+	}
+
+	sum /= 2; // 나누기 2를 하면 전체 사탕의 개 수를 알 수 있다
+
+	for (int i = 0; i < n; i++)
+	{
+		int tmp = 0;
+		for (int j = 0; j < n; j += 2)
+		{
+			tmp += arr[(i + j) % n];
+		}
+
+		cout << tmp - sum << '\n';
+	}
+
+	return 0;
+}
