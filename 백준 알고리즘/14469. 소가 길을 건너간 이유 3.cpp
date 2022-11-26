659ce795fb14f7b9d1771bc0be58cfdf84c5eaeb
AnSungHun <cjswo4577@gmail.com>
2022-11-26 09:23:48 +0900

    14469. 소가 길을 건너간 이유 3 문제 풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - vector 정렬 조건함수 만들고 적용 시켜서 풀이

---
+++ 백준 알고리즘/14469. 소가 길을 건너간 이유 3.cpp
@@ -0,0 +1,56 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+bool cmp(pair<int, int> x, pair<int, int> y)
+{
+    // 시작 시간이 같다면 소요시간이 더 짧은게 앞으로 오도록 한다
+    if (x.first == y.first)
+    {
+        return x.second < y.second;
+    }
+    else
+    {
+        return x.first < y.first; // 그 외엔 시작시간이 짧은게 먼저 오도록
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int n;
+    int cnt = 0;
+    cin >> n;
+    vector<pair<int, int>> arr;
+
+    for (int i = 0; i < n; i++)
+    {
+        int start, tmp;
+        cin >> start >> tmp;
+        pair<int, int> tmpPair = make_pair(start, tmp);
+        arr.push_back(tmpPair);
+    }
+
+    // 비교함수 기준으로 정렬
+    sort(arr.begin(), arr.end(), cmp);
+
+    for (auto now : arr)
+    {
+        // 현재 시간이 first보다 작다면 first로 갱신
+        if (cnt < now.first)
+        {
+            cnt = now.first;
+        }
+        // 소요시간인 second를 더해준다
+        cnt += now.second;
+    }
+
+    cout << cnt;
+    return 0;
+}
