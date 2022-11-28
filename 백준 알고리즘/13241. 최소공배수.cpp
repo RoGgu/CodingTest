7ef7e0ef102fe8542053c7f0be4767dab4e5a2dc
AnSungHun <cjswo4577@gmail.com>
2022-11-28 23:41:43 +0900

    13241. 최소공배수 문제 풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - Gcd 최대공약수 공식으로 구해서 그 수를 토대로 최소공배수를 구한다

---
+++ 백준 알고리즘/13241. 최소공배수.cpp
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+long long Gcd(long long a, long long b)
+{
+    // 최소공배수 구하기
+    if (a < b)
+    {
+        long long tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    long long tmp;
+    while (b != 0)
+    {
+        tmp = a % b;
+        a = b;
+        b = tmp;
+    }
+
+    return a;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    long long a, b, gcd;
+    cin >> a >> b;
+
+    gcd = Gcd(a, b); // 최대공약수를 구한다
+
+    cout << (a * b) / gcd; // 최소공배수는 두 수 곱한거에 / 최대공약수를 하면 구해진다
+    return 0;
+}
