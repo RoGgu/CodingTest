490c9c6c55f2649a7831075d064cc1db6fa4307e
AnSungHun <cjswo4577@gmail.com>
2022-12-02 21:44:12 +0900

    2012. 등수 매기기 문제 풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 오름차순 정렬 후 차이 값 비교해서 풀이

---
+++ 백준 알고리즘/2012. 등수 매기기.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int N;
+    long long res = 0;
+    cin >> N;
+    vector<int> v(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end()); // 오름차순으로 정렬
+
+    for (int i = 1; i <= N; i++)
+    {
+        // 예상 등수 1등에 가까운 사람부터 1등씩줘서 실제 등수 차이를 최소로 한다
+        res += abs(v[i - 1] - i);
+    }
+
+    cout << res;
+    return 0;
+}
