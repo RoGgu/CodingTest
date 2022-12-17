265eb8655f6566ebf46c7fc76807bbd814013f9f
AnSungHun <cjswo4577@gmail.com>
2022-12-17 16:38:25 +0900

    17219. 비밀번호 찾기 문제 풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - map 자료구조 이용해서 사이트, 비밀번호 쌍으로 세팅하고 찾기

---
+++ 백준 알고리즘/17219. 비밀번호 찾기.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<map>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "rt", stdin);
+    int N, M;
+    map<string, string> m;
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        // 사이트, 비밀번호 쌍을 map으로 저장한다
+        pair<string, string> tmp;
+        cin >> tmp.first;
+        cin >> tmp.second;
+
+        m.emplace(tmp);
+    }
+
+    for (int i = 0; i < M; i++)
+    {
+        string s;
+        cin >> s;
+
+        // 사이트 키값에 비밀번호가 기록되어 있다
+        cout << m[s] << endl;
+    }
+
+    return 0;
+}
