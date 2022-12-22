3e1164a08aa141b5e59e79346654a8e4c259c0c3
AnSungHun <cjswo4577@gmail.com>
2022-12-22 23:48:14 +0900

    15655. N과 M (7) 문제 풀이 코드
    - 알고리즘 문제풀이 코드 추가
    - 재귀 이용해서 풀이

---
+++ 백준 알고리즘/15657. N과 M (7).cpp
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+const int MAX = 7;
+
+int N, M;
+int arr[MAX];
+int visited[MAX];
+
+void Fun(int idx)
+{
+	if (idx == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[visited[i]] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		visited[idx] = i;
+		Fun(idx + 1);
+	}
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	Fun(0);
+
+	return 0;
+}
